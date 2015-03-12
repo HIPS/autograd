e4ed1d79263a75066367537b696dcc0fd8fb6f5e
Matthew Johnson <mattjj@csail.mit.edu>
2015-03-12 15:05:18 -0400

    remove side-effects on operator module

--- autograd/node_types.py
+++ autograd/node_types.py
@@ -1,22 +1,22 @@
 import numpy as np
-import operator as op
 from abc import ABCMeta
 from core import Node, Setter, getval, zeros_like
+import grads
 
 class NumericNode(Node):
     __array_priority__ = 100.0 # Ensure precedence of Node's __rmul__ over numpy's __mul__
     __metaclass__ = ABCMeta
-    def __add__(self, other):   return op.add(self, other)
-    def __radd__(self, other):  return op.add(self, other)
-    def __sub__(self, other):   return op.sub(self, other)
-    def __rsub__(self, other):  return op.sub(other, self)
-    def __mul__(self, other):   return op.mul(self, other)
-    def __rmul__(self, other):  return op.mul(other, self)
-    def __neg__(self):          return op.neg(self)
-    def __pow__(self, power):   return op.pow(self, power)
-    def __rpow__(self, power):  return op.pow(power, self)
-    def __div__(self, other):   return op.div(self, other)
-    def __rdiv__(self, other):  return op.div(other, self)
+    def __add__(self, other):   return grads.add(self, other)
+    def __radd__(self, other):  return grads.add(self, other)
+    def __sub__(self, other):   return grads.sub(self, other)
+    def __rsub__(self, other):  return grads.sub(other, self)
+    def __mul__(self, other):   return grads.mul(self, other)
+    def __rmul__(self, other):  return grads.mul(other, self)
+    def __neg__(self):          return grads.neg(self)
+    def __pow__(self, power):   return grads.pow(self, power)
+    def __rpow__(self, power):  return grads.pow(power, self)
+    def __div__(self, other):   return grads.div(self, other)
+    def __rdiv__(self, other):  return grads.div(other, self)
     def __lt__(self, other):    return getval(self) < getval(other)
     def __gt__(self, other):    return getval(self) > getval(other) 
 
