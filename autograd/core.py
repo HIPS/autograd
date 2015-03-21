0ed107fd6139cc481cc34a050e56cd7a85f4064e
Dougal <d.maclaurin@gmail.com>
2015-03-21 16:20:02 -0400

    More refactoring.
    Reinstated Node type. Getting rid of it led to endless sources of silent failure which
    were very hard to debug. But this time nodes aren't nestable.

--- autograd/core.py
+++ autograd/core.py
@@ -3,100 +3,106 @@ import warnings
 import operator as op
 from operator import attrgetter
 from itertools import count
-from numpy import log
+from numpy import log, float64
 
 def grad(fun, argnum=0):
     def gradfun(*args, **kwargs):
         tape = CalculationTape()
-        start_node = tape.add_node(args[argnum])
+        start_node = new_node(args[argnum])
+        start_node.reverse_nodes[tape] = ReverseNode(tape, start_node)
         args = args[:argnum] + (start_node,) + args[argnum+1:]
         end_node = fun(*args, **kwargs)
-        tape.deactivate()
-        if not tape.hasmember(end_node):
+        if not (isinstance(end_node, Node) and tape in end_node.reverse_nodes):
             warnings.warn("Output seems independent of input. Returning zero gradient.")
-            return 0 * args[argnum]
-        elif not isinstance(end_node, float):
+            return 0 * start_node.value
+        elif not isinstance(end_node.value, float):
             raise TypeError("Can only take gradient of scalar-valued functions")
         else:
-            tape.lookup(end_node).outgrad = 1.0
+            end_node.reverse_nodes[tape].outgrad = 1.0
+            tape.finalize()
             for node in tape[::-1]:
                 if node.outgrad is not 0:
                     for gradfun, parent in node.parent_ops:
                         parent.outgrad = parent.outgrad + gradfun(node.outgrad)
             return tape[0].outgrad
-
     return gradfun
 
-def grad_not_implemented(*args, **kwargs):
-    raise NotImplementedError("Gradient not implemented yet")
+getval = lambda x : x.value if isinstance(x, Node) else x
 
-def primitive(fun, gradmaker=grad_not_implemented):
+def primitive(fun, gradmaker):
     def wrapped_function(*args, **kwargs):
-        result = fun(*args, **kwargs)
-        all_parent_ops = []
-        for tape in CalculationTape.active_tapes[::-1]:
-            parent_ops = [(gradmaker(result, *args, **kwargs)[i], tape.lookup(parent))
-                          for i, parent in enumerate(args) if tape.hasmember(parent)]
-            if parent_ops:
-                result = tape.add_node(result, parent_ops)
+        result = fun(*map(getval, args), **kwargs)
+        for i, arg in enumerate(args):
+            if isinstance(arg, Node):
+                for tape in arg.reverse_nodes:
+                    result = new_node(result)
+                    gradfun = gradmaker(result, *args, **kwargs)[i]
+                    result.add_reverse_op(tape, (gradfun, arg.reverse_nodes[tape]))
         return result
     return wrapped_function
 
-class CalculationTape(list):
-    tape_count = count(0)
-    active_tapes = []
-    def __init__(self):
-        super(CalculationTape, self).__init__([])
-        self.obj_lookup = {}
-        self.priority = self.tape_count.next()
-        self.active_tapes.append(self)
+def new_node(value):
+    if isinstance(value, Node):
+        return value
+    else:
+        return Node(value)
 
-    def hasmember(self, x):
-        return id(x) in self.obj_lookup
+class Node(object):
+    type_mappings = {}
+    def __new__(cls, value):
+        try:
+            subclass = Node.type_mappings[type(value)]
+            return super(Node, cls).__new__(subclass, value)
+        except KeyError:
+            raise TypeError("Can't differentiate wrt {0}".format(type(value)))
 
-    def lookup(self, node):
-        return self[self.obj_lookup[id(node)]]
+    def __init__(self, value):
+        self.value = value
+        self.reverse_nodes = {}
 
-    def add_node(self, node, parent_ops = []):
-        if isinstance(node, float) and not isinstance(node, FloatNode):
-            node = FloatNode(node)
-        self.append(ReverseNode(node, parent_ops))
-        self.obj_lookup[id(node)] = len(self) - 1
-        return node
-
-    def deactivate(self):
-        for i, tape in enumerate(self.active_tapes):
-            if tape is self:
-                del(self.active_tapes[i])
-
-def add_tape(node, tape, ops=None):
-    if isinstance(node, float):
-        node = FloatNode(node)
-    return node.add_tape(tape, ops)
+    def add_reverse_op(self, tape, reverse_op):
+        if tape not in self.reverse_nodes:
+            self.reverse_nodes[tape] = ReverseNode(tape, self)
+        self.reverse_nodes[tape].parent_ops.append(reverse_op)
 
 class ReverseNode(object):
-    __slots__ = ['parent_ops', 'outgrad', 'node']
-    def __init__(self, node, parent_ops):
-        self.node = node
-        self.parent_ops = parent_ops
+    __slots__ = ['parent_ops', 'outgrad', 'tape', 'node']
+    def __init__(self, tape, node):
+        self.tape = tape
+        self.tape.append(self)
+        self.parent_ops = []
         self.outgrad = 0
+        self.node = node
 
+    def remove_self_from_node(self):
+        del self.node.reverse_nodes[self.tape]
+
+class CalculationTape(list):
+    def finalize(self):
+        for rnode in self:
+            rnode.remove_self_from_node()
+
+    def __hash__(self):
+        return id(self)
+
+P = primitive
 I = lambda x : x # Identity operator
-grad_neg = lambda ans, x    : [neg]
+grad_neg = lambda ans, x    : [op.neg]
 grad_add = lambda ans, x, y : [I, I]
 grad_mul = lambda ans, x, y : [lambda g : y * g, lambda g : x * g]
-grad_sub = lambda ans, x, y : [I, neg]
+grad_sub = lambda ans, x, y : [I, op.neg]
 grad_div = lambda ans, x, y : [lambda g : g / y, lambda g : - g * x / y**2]
 grad_pow = lambda ans, x, y : [lambda g : g * y * x ** (y - 1),
                                lambda g : g * log(x) * x ** y]
 grad_log = lambda ans, x    : [lambda g : g / x]
+log = P(log, grad_log)
+
 def reverse_args(fun):
     def new_fun(ans, x, y):
         return fun(ans, y, x)[::-1]
     return new_fun
 
-P = primitive
-class FloatNode(float):
+class FloatNode(Node):
     __add__  = P(float.__add__ , grad_add)
     __radd__ = P(float.__radd__, reverse_args(grad_add))
     __sub__  = P(float.__sub__,  grad_sub)
@@ -108,3 +114,5 @@ class FloatNode(float):
     __div__  = P(float.__div__,  grad_div)
     __rdiv__ = P(float.__rdiv__, reverse_args(grad_div))
     __neg__  = P(float.__neg__,  grad_neg)
+Node.type_mappings[float] = FloatNode
+Node.type_mappings[float64] = FloatNode
--- autograd/numpy/numpy_core.py
+++ autograd/numpy/numpy_core.py
@@ -3,8 +3,7 @@ from functools import partial
 from copy import copy
 from numpy import *
 import numpy as np_orig
-# from autograd.core import primitive, getval, Node
-# from autograd.basic_ops import log, NumericNode
+from autograd.core import primitive, Node, log, getval
 
 # ----- Objects in numpy.__dict__ not imported by * -----
 
@@ -23,186 +22,186 @@ round   = np_orig.round
 
 # ----- Broadcasting logic -----
 
-# isarray = lambda x : isinstance(getval(x), ndarray)
-# isfloat = lambda x : isinstance(getval(x), float)
+isarray = lambda x : isinstance(getval(x), ndarray)
+isfloat = lambda x : isinstance(getval(x), float)
 
-# def do_unbroadcast(ans, x, y, funs):
-#     return [unbroadcast_fun(ans, x, funs[0]),
-#             unbroadcast_fun(ans, y, funs[1])]
+def do_unbroadcast(ans, x, y, funs):
+    return [unbroadcast_fun(ans, x, funs[0]),
+            unbroadcast_fun(ans, y, funs[1])]
 
-# def unbroadcast_fun(ans, x, fun):
-#     if isfloat(x) and isarray(ans):
-#         return lambda g : sum(fun(g))
-#     elif isarray(x):
-#         shape = x.shape
-#         def new_fun(g):
-#             result = fun(g)
-#             while result.ndim > len(shape):
-#                 result = sum(result, axis=0)
-#             for axis, size in enumerate(shape):
-#                 if size is 1:
-#                     result = sum(result, axis, keepdims=True)
-#             return result
-#         return new_fun
-#     else:
-#         return fun
+def unbroadcast_fun(ans, x, fun):
+    if isfloat(x) and isarray(ans):
+        return lambda g : sum(fun(g))
+    elif isarray(x):
+        shape = x.shape
+        def new_fun(g):
+            result = fun(g)
+            while result.ndim > len(shape):
+                result = sum(result, axis=0)
+            for axis, size in enumerate(shape):
+                if size is 1:
+                    result = sum(result, axis, keepdims=True)
+            return result
+        return new_fun
+    else:
+        return fun
 
-# # ----- Node version of ndarray -----
+# ----- Node version of ndarray -----
 
-# class ArrayNode(NumericNode):
-#     def __getitem__(self, idx):
-#         return take(self, idx)
-#     def reshape(self, shape, order=None):
-#         return reshape(self, shape, order=order)
-#     def ravel(self, order=None):
-#         return ravel(self, order=order)
-#     def squeeze(self, axis=None):
-#         return squeeze(self, axis=axis)
-#     def sum(self):
-#         return sum(self)
-#     @property
-#     def T(self): return transpose(self)
-#     @property
-#     def shape(self): return self.value.shape
-#     @property
-#     def ndim(self): return self.value.ndim
-#     @property
-#     def size(self): return self.value.size
-# Node.add_subclass(ArrayNode, [ndarray])
+class ArrayNode(Node):
+    def __getitem__(self, idx):
+        return take(self, idx)
+    def reshape(self, shape, order=None):
+        return reshape(self, shape, order=order)
+    def ravel(self, order=None):
+        return ravel(self, order=order)
+    def squeeze(self, axis=None):
+        return squeeze(self, axis=axis)
+    def sum(self):
+        return sum(self)
+    @property
+    def T(self): return transpose(self)
+    @property
+    def shape(self): return self.value.shape
+    @property
+    def ndim(self): return self.value.ndim
+    @property
+    def size(self): return self.value.size
+Node.type_mappings[ndarray] = ArrayNode
 
-# # ----- Sparse array -----
+# ----- Sparse array -----
 
-# class SparseArray(object):
-#     __array_priority__ = 50.0
-#     def __init__(self, shape, idx, val):
-#         self.shape = shape
-#         self.idx = idx
-#         self.val = val
+class SparseArray(object):
+    __array_priority__ = 50.0
+    def __init__(self, shape, idx, val):
+        self.shape = shape
+        self.idx = idx
+        self.val = val
 
-#     def __add__(self, other):
-#         array = zeros(self.shape) if other is 0 else copy(other)
-#         array[self.idx] += self.val
-#         return array
+    def __add__(self, other):
+        array = zeros(self.shape) if other is 0 else copy(other)
+        array[self.idx] += self.val
+        return array
 
-#     def __radd__(self, other):
-#         return self + other
+    def __radd__(self, other):
+        return self + other
 
-# class SparseArrayNode(NumericNode):
-#     pass
-# Node.add_subclass(SparseArrayNode, [SparseArray])
+class SparseArrayNode(Node):
+    pass
+Node.type_mappings[SparseArray] = SparseArrayNode
 
-# take = lambda A, idx : A[idx]
-# def make_grad_take(ans, A, idx):
-#     shape = A.shape
-#     return [lambda g : untake(g, idx, shape)]
-# take = primitive(take, make_grad_take)
+take = lambda A, idx : A[idx]
+def make_grad_take(ans, A, idx):
+    shape = A.shape
+    return [lambda g : untake(g, idx, shape)]
+take = primitive(take, make_grad_take)
 
-# untake = lambda x, idx, shape : SparseArray(shape, idx, x)
-# untake = primitive(untake, lambda ans, x, idx, shape : [lambda g : take(g, idx)])
+untake = lambda x, idx, shape : SparseArray(shape, idx, x)
+untake = primitive(untake, lambda ans, x, idx, shape : [lambda g : take(g, idx)])
 
-# # ----- Numpy gradients -----
+# ----- Numpy gradients -----
 
-# P = primitive
-# isarray = lambda x : isinstance(getval(x), ndarray)
-# I = lambda x : x # Identity operator
+P = primitive
+isarray = lambda x : isinstance(getval(x), ndarray)
+I = lambda x : x # Identity operator
 
-# abs    = P(abs,    lambda ans, x : [lambda g : sign(x) * g])
-# exp    = P(exp,    lambda ans, x : [lambda g : ans * g])
-# sin    = P(sin,    lambda ans, x : [lambda g : g * cos(x)])
-# cos    = P(cos,    lambda ans, x : [lambda g : - g * sin(x)])
-# tan    = P(tan,    lambda ans, x : [lambda g : g / cos(x) **2])
-# sinh   = P(sinh,   lambda ans, x : [lambda g : g * cosh(x)])
-# cosh   = P(cosh,   lambda ans, x : [lambda g : g * sinh(x)])
-# tanh   = P(tanh,   lambda ans, x : [lambda g : g / cosh(x) **2])
-# square = P(square, lambda ans, x : [lambda g : g * 2 * x])
-# sqrt   = P(sqrt,   lambda ans, x : [lambda g : g * 0.5 * x**-0.5])
-# sign   = P(sign,   lambda ans, x : [lambda g : 0.0])
-# full   = P(full,   lambda ans, shape, fill_value : [None, lambda g :  sum(g)])
-# reshape     = P(reshape,     lambda ans, x, shape, order=None : [lambda g : reshape(g, x.shape, order=order)])
-# ravel       = P(ravel,       lambda ans, x, order=None   : [lambda g : reshape(g, x.shape, order=order)])
-# expand_dims = P(expand_dims, lambda ans, x, axis         : [lambda g : squeeze(g, axis)])
-# squeeze     = P(squeeze,     lambda ans, x, axis         : [lambda g : repeat(g, x.shape[axis], axis)])
-# repeat      = P(repeat,      lambda ans, x, shape, axis  : [lambda g : sum(g, axis, keepdims=True)])
-# transpose   = P(transpose,   lambda ans, x               : [lambda g : transpose(g)])
-# split       = P(split,       lambda ans, x, idxs, axis=0 : [lambda g : concatenate(g, axis=axis)])
-# diag        = P(diag,        lambda ans, x               : [lambda g : diag(g)])
-# trace       = P(trace,       lambda ans, x               : [lambda g : g * eye(x.shape[0])])
+abs    = P(abs,    lambda ans, x : [lambda g : sign(x) * g])
+exp    = P(exp,    lambda ans, x : [lambda g : ans * g])
+sin    = P(sin,    lambda ans, x : [lambda g : g * cos(x)])
+cos    = P(cos,    lambda ans, x : [lambda g : - g * sin(x)])
+tan    = P(tan,    lambda ans, x : [lambda g : g / cos(x) **2])
+sinh   = P(sinh,   lambda ans, x : [lambda g : g * cosh(x)])
+cosh   = P(cosh,   lambda ans, x : [lambda g : g * sinh(x)])
+tanh   = P(tanh,   lambda ans, x : [lambda g : g / cosh(x) **2])
+square = P(square, lambda ans, x : [lambda g : g * 2 * x])
+sqrt   = P(sqrt,   lambda ans, x : [lambda g : g * 0.5 * x**-0.5])
+sign   = P(sign,   lambda ans, x : [lambda g : 0.0])
+full   = P(full,   lambda ans, shape, fill_value : [None, lambda g :  sum(g)])
+reshape     = P(reshape,     lambda ans, x, shape, order=None : [lambda g : reshape(g, x.shape, order=order)])
+ravel       = P(ravel,       lambda ans, x, order=None   : [lambda g : reshape(g, x.shape, order=order)])
+expand_dims = P(expand_dims, lambda ans, x, axis         : [lambda g : squeeze(g, axis)])
+squeeze     = P(squeeze,     lambda ans, x, axis         : [lambda g : repeat(g, x.shape[axis], axis)])
+repeat      = P(repeat,      lambda ans, x, shape, axis  : [lambda g : sum(g, axis, keepdims=True)])
+transpose   = P(transpose,   lambda ans, x               : [lambda g : transpose(g)])
+split       = P(split,       lambda ans, x, idxs, axis=0 : [lambda g : concatenate(g, axis=axis)])
+diag        = P(diag,        lambda ans, x               : [lambda g : diag(g)])
+trace       = P(trace,       lambda ans, x               : [lambda g : g * eye(x.shape[0])])
 
-# # ----- Subtler gradients -----
+# ----- Subtler gradients -----
 
-# def make_grad_np_sum(ans, x, axis=None, keepdims=False):
-#     if not isarray(x):
-#         return [I]
-#     shape = x.shape
-#     if axis is None:
-#         return [lambda g : full(shape, g)]
-#     else:
-#         if keepdims:
-#             return [lambda g : repeat(g, shape[axis], axis)]
-#         else:
-#             return [lambda g : repeat(expand_dims(g, axis),
-#                                       shape[axis], axis)]
-# sum = P(sum, make_grad_np_sum)
+def make_grad_np_sum(ans, x, axis=None, keepdims=False):
+    if not isarray(x):
+        return [I]
+    shape = x.shape
+    if axis is None:
+        return [lambda g : full(shape, g)]
+    else:
+        if keepdims:
+            return [lambda g : repeat(g, shape[axis], axis)]
+        else:
+            return [lambda g : repeat(expand_dims(g, axis),
+                                      shape[axis], axis)]
+sum = P(sum, make_grad_np_sum)
 
-# def make_grad_np_mean(ans, x, axis=None, keepdims=False):
-#     if not isarray(x):
-#         return [I]
-#     shape = x.shape
-#     if axis is None:
-#         return [lambda g : full(shape, g) / prod(shape)]
-#     else:
-#         if keepdims:
-#             return [lambda g : repeat(g, shape[axis], axis) / shape[axis]]
-#         else:
-#             return [lambda g : repeat(expand_dims(g, axis),
-#                                       shape[axis], axis) / shape[axis]]
-# mean = P(mean, make_grad_np_mean)
+def make_grad_np_mean(ans, x, axis=None, keepdims=False):
+    if not isarray(x):
+        return [I]
+    shape = x.shape
+    if axis is None:
+        return [lambda g : full(shape, g) / prod(shape)]
+    else:
+        if keepdims:
+            return [lambda g : repeat(g, shape[axis], axis) / shape[axis]]
+        else:
+            return [lambda g : repeat(expand_dims(g, axis),
+                                      shape[axis], axis) / shape[axis]]
+mean = P(mean, make_grad_np_mean)
 
-# def make_grad_np_max(ans, x):
-#     def gradfun(g):
-#         idxs = argmax(getval(x))
-#         shape = x.shape
-#         return untake(g, unravel_index(idxs, shape), shape)
-#     return [gradfun]
-# max = P(max, make_grad_np_max)
+def make_grad_np_max(ans, x):
+    def gradfun(g):
+        idxs = argmax(getval(x))
+        shape = x.shape
+        return untake(g, unravel_index(idxs, shape), shape)
+    return [gradfun]
+max = P(max, make_grad_np_max)
 
-# def make_grad_np_dot(ans, A, B):
-#     def grad_np_dot_A(g):
-#         if B.ndim is 2:
-#             return dot(g, B.T)
-#         elif A.ndim is 2:
-#             return outer(g, B)
-#         else:
-#             return g * B
-#     def grad_np_dot_B(g):
-#         if A.ndim is 2:
-#             return dot(A.T, g)
-#         elif B.ndim is 2:
-#             return outer(A, g)
-#         else:
-#             return g * A
-#     return [grad_np_dot_A, grad_np_dot_B]
-# dot = P(dot, make_grad_np_dot)
+def make_grad_np_dot(ans, A, B):
+    def grad_np_dot_A(g):
+        if B.ndim is 2:
+            return dot(g, B.T)
+        elif A.ndim is 2:
+            return outer(g, B)
+        else:
+            return g * B
+    def grad_np_dot_B(g):
+        if A.ndim is 2:
+            return dot(A.T, g)
+        elif B.ndim is 2:
+            return outer(A, g)
+        else:
+            return g * A
+    return [grad_np_dot_A, grad_np_dot_B]
+dot = P(dot, make_grad_np_dot)
 
-# concatenate_orig = concatenate
-# def concatenate_args(axis, *args):
-#     return concatenate_orig(args, axis)
-# def make_grad_concatenate_args(ans, axis, *args):
-#     high = 0
-#     gradfuns = [None]
-#     for a in args:
-#         low = high
-#         high += a.shape[axis]
-#         idxs = [slice(None)] * ans.ndim
-#         idxs[axis] = slice(low, high)
-#         gradfuns.append(partial(take, idx=idxs))
-#     return gradfuns
-# concatenate_args = P(concatenate_args, make_grad_concatenate_args)
-# concatenate = lambda arr_list, axis=0 : concatenate_args(axis, *arr_list)
+concatenate_orig = concatenate
+def concatenate_args(axis, *args):
+    return concatenate_orig(args, axis)
+def make_grad_concatenate_args(ans, axis, *args):
+    high = 0
+    gradfuns = [None]
+    for a in args:
+        low = high
+        high += a.shape[axis]
+        idxs = [slice(None)] * ans.ndim
+        idxs[axis] = slice(low, high)
+        gradfuns.append(partial(take, idx=idxs))
+    return gradfuns
+concatenate_args = P(concatenate_args, make_grad_concatenate_args)
+concatenate = lambda arr_list, axis=0 : concatenate_args(axis, *arr_list)
 
-# def make_grad_np_concatenate(ans, arr_list, axis=0):
-#     idxs = cumsum([a.shape[axis] for a in arr_list[:-1]])
-#     def grad_np_concatenate(g):
-#         return split(g, idxs, axis=axis)
-#     return [grad_np_concatenate]
-# concatenate = P(concatenate, make_grad_np_concatenate)
+def make_grad_np_concatenate(ans, arr_list, axis=0):
+    idxs = cumsum([a.shape[axis] for a in arr_list[:-1]])
+    def grad_np_concatenate(g):
+        return split(g, idxs, axis=axis)
+    return [grad_np_concatenate]
+concatenate = P(concatenate, make_grad_np_concatenate)
