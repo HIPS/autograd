88eb3035efba3b0e869b5884ca5c4982101423d8
Dougal <d.maclaurin@gmail.com>
2015-03-21 19:01:15 -0400

    Now have mean and sum working. Had to use an ugly workaround because of the way numpy handles keepdims for subclasses.

--- autograd/numpy/numpy_core.py
+++ autograd/numpy/numpy_core.py
@@ -41,8 +41,8 @@ def unbroadcast_fun(ans, x, fun):
                 result = sum(result, axis=0)
             for axis, size in enumerate(shape):
                 if size is 1:
-                    # Numpy doesn't support keepdims for subclasses so this is the workaround
-                    result = result.sum(axis, keepdims=True)
+                    result = sum(result, axis=axis, keepdims=True)
+            assert result.shape == shape
             return result
         return new_fun
     else:
@@ -60,9 +60,30 @@ def make_grad_np_sum(ans, x, axis=None, keepdims=False):
         else:
             return [lambda g : repeat(expand_dims(g, axis),
                                       shape[axis], axis)]
-sum = primitive(sum, make_grad_np_sum)
 
-# ----- Node version of ndarray -----
+# Numpy doesn't support keepdims for subclasses so this is the workaround
+def keep_keepdims(fun, funname):
+    def new_fun(*args, **kwargs):
+        x = args[0]
+        return getattr(x, funname)(*args[1:], **kwargs) if isinstance(x, np_orig.ndarray) else x
+    return new_fun
+sum = primitive(keep_keepdims(sum, 'sum'), make_grad_np_sum)
+
+def make_grad_np_mean(ans, x, axis=None, keepdims=False):
+    if not isarray(x):
+        return [I]
+    shape = x.shape
+    if axis is None:
+        return [lambda g : full(shape, g) / prod(shape)]
+    else:
+        if keepdims:
+            return [lambda g : repeat(g, shape[axis], axis) / shape[axis]]
+        else:
+            return [lambda g : repeat(expand_dims(g, axis),
+                                      shape[axis], axis) / shape[axis]]
+mean = primitive(keep_keepdims(mean, 'mean'), make_grad_np_mean)
+
+# ----- Slightly modified version of ndarray -----
 
 P = primitive
 I = lambda x : x # Identity operator
@@ -97,41 +118,6 @@ class ndarray(np_orig.ndarray):
     __rmul__ = P(np_orig.ndarray.__rmul__, reverse_args(grad_mul))
     __rpow__ = P(np_orig.ndarray.__rpow__, reverse_args(grad_pow))
     __rdiv__ = P(np_orig.ndarray.__rdiv__, reverse_args(grad_div))
-
-class ArrayNode(Node):
-    # Constants w.r.t float data just pass though
-    @property
-    def shape(self): return self.value.shape
-    @property
-    def ndim(self): return self.value.ndim
-    @property
-    def size(self): return self.value.size
-    # Differentiable unary methods and properties need to be wrapped
-    def squeeze(self, axis=None):
-        return squeeze(self, axis=axis)
-    def ravel(self, order=None):
-         return ravel(self, order=order)
-    def reshape(self, shape, order=None):
-        return reshape(self, shape, order=order)
-    sum = primitive(ndarray.sum, make_grad_np_sum)
-
-    @property
-    def T(self): return transpose(self)
-    __neg__ = P(op.neg,  grad_neg)
-
-    # Binary ops already wrapped by autograd.numpy.ndarray
-    __add__  = ndarray.__add__.__func__
-    __sub__  = ndarray.__sub__.__func__
-    __mul__  = ndarray.__mul__.__func__
-    __pow__  = ndarray.__pow__.__func__
-    __div__  = ndarray.__div__.__func__
-    __radd__ = ndarray.__radd__.__func__
-    __rsub__ = ndarray.__rsub__.__func__
-    __rmul__ = ndarray.__rmul__.__func__
-    __rpow__ = ndarray.__rpow__.__func__
-    __rdiv__ = ndarray.__rdiv__.__func__
-
-Node.type_mappings[ndarray] = ArrayNode
 
 # ----- Sparse array -----
 
@@ -204,20 +190,6 @@ trace       = P(trace,       lambda ans, x               : [lambda g : g * eye(x
 
 # ----- Subtler gradients -----
 
-def make_grad_np_mean(ans, x, axis=None, keepdims=False):
-    if not isarray(x):
-        return [I]
-    shape = x.shape
-    if axis is None:
-        return [lambda g : full(shape, g) / prod(shape)]
-    else:
-        if keepdims:
-            return [lambda g : repeat(g, shape[axis], axis) / shape[axis]]
-        else:
-            return [lambda g : repeat(expand_dims(g, axis),
-                                      shape[axis], axis) / shape[axis]]
-mean = P(mean, make_grad_np_mean)
-
 def make_grad_np_max(ans, x):
     def gradfun(g):
         idxs = argmax(getval(x))
@@ -259,3 +231,38 @@ def make_grad_concatenate_args(ans, axis, *args):
     return gradfuns
 concatenate_args = P(W(concatenate_args), make_grad_concatenate_args)
 concatenate = lambda arr_list, axis=0 : concatenate_args(axis, *arr_list)
+
+# ----- Node version of ndarray -----
+
+class ArrayNode(Node):
+    # Constants w.r.t float data just pass though
+    @property
+    def shape(self): return self.value.shape
+    @property
+    def ndim(self): return self.value.ndim
+    @property
+    def size(self): return self.value.size
+
+    # Differentiable unary methods just apply to self
+    squeeze = squeeze
+    ravel = ravel
+    reshape = reshape
+    sum = sum
+    mean = mean
+
+    @property
+    def T(self): return transpose(self)
+    __neg__ = P(op.neg,  grad_neg)
+
+    # Binary ops already wrapped by autograd.numpy.ndarray
+    __add__  = ndarray.__add__.__func__
+    __sub__  = ndarray.__sub__.__func__
+    __mul__  = ndarray.__mul__.__func__
+    __pow__  = ndarray.__pow__.__func__
+    __div__  = ndarray.__div__.__func__
+    __radd__ = ndarray.__radd__.__func__
+    __rsub__ = ndarray.__rsub__.__func__
+    __rmul__ = ndarray.__rmul__.__func__
+    __rpow__ = ndarray.__rpow__.__func__
+    __rdiv__ = ndarray.__rdiv__.__func__
+Node.type_mappings[ndarray] = ArrayNode
