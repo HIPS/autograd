1a18d683cc2f9e3c9d9ae81c1dcdb589b94991c3
Dougal <d.maclaurin@gmail.com>
2015-03-23 19:34:01 -0400

    Importing numpy namespace with loops

--- autograd/numpy/numpy_wrapper.py
+++ autograd/numpy/numpy_wrapper.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 import numpy as np
-from autograd.core import primitive as P
-
-# ----- Wrap numpy functions -----
+from autograd.core import primitive
 
 def keep_keepdims(fun, funname):
     # Numpy doesn't support keepdims for subclasses so this is the workaround
@@ -20,46 +18,28 @@ def wrap_output(fun):
         return ans
     return wrapped_fun
 
-W = wrap_output
-# Differentiable functions
-abs    = P(np.abs)
-exp    = P(np.exp)
-log    = P(np.log)
-sin    = P(np.sin)
-cos    = P(np.cos)
-tan    = P(np.tan)
-sinh   = P(np.sinh)
-cosh   = P(np.cosh)
-tanh   = P(np.tanh)
-square = P(np.square)
-sqrt   = P(np.sqrt)
-sign   = P(np.sign)
-full   = P(W(np.full))
-reshape  = P(np.reshape)
-ravel    = P(W(np.ravel))
-expand_dims = P(W(np.expand_dims))
-squeeze     = P(np.squeeze)
-repeat      = P(np.repeat)
-transpose   = P(np.transpose)
-split       = P(np.split)
-diag        = P(W(np.diag))
-trace       = P(np.trace)
-sum         = P(keep_keepdims(np.sum,  'sum'))
-max         = P(np.max)
-mean        = P(keep_keepdims(np.mean, 'mean'))
-dot         = P(np.dot)
-prod  = P(np.prod)
-outer = P(W(np.outer))
+grad_only = ['abs', 'exp', 'log', 'sin', 'cos', 'tan', 'sinh', 'cosh', 'tanh',
+             'square', 'sqrt', 'sign', 'reshape', 'max', 'dot', 'prod',
+             'squeeze', 'repeat', 'transpose', 'split', 'trace']
+direct_import = ['float64', 'allclose', 'round', 'argmax', 'unravel_index']
+grad_and_wrap = ['outer', 'full', 'ravel', 'expand_dims', 'diag']
+grad_and_keepdims = ['sum', 'mean']
+wrap_only = ['zeros', 'ones', 'eye']
 
-# Functions constant w.r.t. real-valued inputs
-float64 = np.float64
-allclose = np.allclose
-round = np.round
-argmax = np.argmax
-unravel_index = np.unravel_index
-zeros = W(np.zeros)
-ones  = W(np.ones)
-eye   = W(np.eye)
+for x in grad_only:
+    globals()[x] = primitive(np.__dict__[x])
+
+for x in direct_import:
+    globals()[x] = np.__dict__[x]
+
+for x in grad_and_wrap:
+    globals()[x] = primitive(wrap_output(np.__dict__[x]))
+
+for x in grad_and_keepdims:
+    globals()[x] = primitive(keep_keepdims(np.__dict__[x], x))
+
+for x in wrap_only:
+    globals()[x] = wrap_output(np.__dict__[x])
 
 # ----- Slightly modified version of ndarray -----
 
@@ -71,18 +51,18 @@ class ndarray(np.ndarray):
             return np.ndarray.__array_wrap__(self, obj)
 
     # Wrap binary ops since the other operand could be a Node
-    __add__  = P(np.ndarray.__add__)
-    __sub__  = P(np.ndarray.__sub__)
-    __mul__  = P(np.ndarray.__mul__)
-    __pow__  = P(np.ndarray.__pow__)
-    __div__  = P(np.ndarray.__div__)
-    __radd__ = P(np.ndarray.__radd__)
-    __rsub__ = P(np.ndarray.__rsub__)
-    __rmul__ = P(np.ndarray.__rmul__)
-    __rpow__ = P(np.ndarray.__rpow__)
-    __rdiv__ = P(np.ndarray.__rdiv__)
+    __add__  = primitive(np.ndarray.__add__)
+    __sub__  = primitive(np.ndarray.__sub__)
+    __mul__  = primitive(np.ndarray.__mul__)
+    __pow__  = primitive(np.ndarray.__pow__)
+    __div__  = primitive(np.ndarray.__div__)
+    __radd__ = primitive(np.ndarray.__radd__)
+    __rsub__ = primitive(np.ndarray.__rsub__)
+    __rmul__ = primitive(np.ndarray.__rmul__)
+    __rpow__ = primitive(np.ndarray.__rpow__)
+    __rdiv__ = primitive(np.ndarray.__rdiv__)
 
 # ----- Special treatment of list-input functions -----
 
-concatenate_args = P(W(lambda axis, *args : np.concatenate(args, axis)))
+concatenate_args = primitive(wrap_outputlambda axis, *args : np.concatenate(args, axis)))
 concatenate = lambda arr_list, axis=0 : concatenate_args(axis, *arr_list)
