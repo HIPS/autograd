de355356c82b955c59a0f97a98bca317cfcdc0b2
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-03-25 12:51:42 -0400

    Made smoke get blown into a peace sign

--- examples/fluidsim/fluidsim.py
+++ examples/fluidsim/fluidsim.py
@@ -2,22 +2,30 @@ import autograd.numpy as np
 from autograd import grad
 
 from scipy.optimize import fmin_cg
+from scipy.misc import imread
 
 import matplotlib.pyplot as plt
+import os
 
-# Based on http://www.intpowertechcorp.com/GDC03.pdf
-
-rows = 100
-cols = 100
+rows = 110
+cols = 110
 dt = 3.1
 num_timesteps = 25
-num_solver_iters = 5
+num_solver_iters = 100
 
-def plot_matrix(ax, mat):
+def plot_matrix(ax, mat, t, render=False):
     plt.cla()
-    ax.matshow(mat)
+    ax.matshow(mat, vmin=0.0, vmax= 1.0)
+    ax.set_xticks([])
+    ax.set_yticks([])
     plt.draw()
+    if render:
+        plt.savefig('step{0:03d}.png'.format(t), bbox_inches='tight')
     plt.pause(0.001)
+
+# Fluid simulation code based on
+# "Real-Time Fluid Dynamics for Games" by Jos Stam
+# http://www.intpowertechcorp.com/GDC03.pdf
 
 def project(vx, vy):
     """Project the velocity field to be mass-conserving,
@@ -57,33 +65,40 @@ def advect(f, vx, vy):
                  + rw * ((1 - bw)*f[right_ix, top_ix] + bw*f[right_ix, bot_ix])
     return np.reshape(flat_f, (rows, cols))
 
-def simulate(vx, vy, smoke, num_time_steps, ax=None):
+def simulate(vx, vy, smoke, num_time_steps, ax=None, render=False):
     """Simulate a fluid for a number of time steps."""
     for t in xrange(num_time_steps):
         vx_updated = advect(vx, vx, vy)
         vy_updated = advect(vy, vx, vy)
         vx, vy = project(vx_updated, vy_updated)
         smoke = advect(smoke, vx, vy)
-        if ax: plot_matrix(ax, smoke)
+        if ax: plot_matrix(ax, smoke, t, render)
     return smoke
 
-def target_match(smoke):
-    """Compute distance from target image."""
-    target = np.zeros((rows, cols))
-    target[30:60, 30:60] = 1.0
-    return np.sum(target * smoke)
 
 if __name__ == '__main__':
 
-    init_dx_and_dy = np.zeros((2, rows, cols)).ravel()
-    #init_dx = np.random.randn(rows, cols) * 3.
-    #init_dy = np.random.randn(rows, cols) * 3.
+    np.random.seed(1)
+    #init_dx_and_dy = np.zeros((2, rows, cols)).ravel()
+    init_dx_and_dy = np.random.randn(2, rows, cols) * 0.01
+    #init_dy = np.random.randn(rows, cols)
     init_smoke = np.zeros((rows, cols))
     init_smoke[10:20:, :] = 1.0
     init_smoke[50:60:, :] = 1.0
+    init_smoke[90:100:, :] = 1.0
 
-    fig = plt.figure(figsize=(12,10))
-    ax = fig.add_axes([0., 0., 1., 1.], frameon=False)
+    target = imread('peace.png')[::2,::2,3].view(np.ndarray)
+
+    #fig = plt.figure(figsize=(12,10))
+    #ax = fig.add_axes([0., 0., 1., 1.], frameon=False)
+    #plt.matshow(target)
+    #plt.show()
+
+    def target_match(smoke):
+        """Compute distance from target image."""
+        #target = np.zeros((rows, cols))
+        #target[30:60, 30:60] = 1.0
+        return np.sum((target - smoke)**2)
 
     #simulate(init_dx, init_dy, init_smoke, num_timesteps, ax)
     def objective(init_vx_and_vy):
@@ -99,15 +114,21 @@ if __name__ == '__main__':
     #g = grad_obj(init_dx_and_dy, init_smoke, num_timesteps)
     #plot_matrix(ax, g[0])
 
+    fig = plt.figure(figsize=(4,4))
+    #ax = fig.add_axes([0., 0., 1., 1.], frameon=False)
+    ax = fig.add_subplot(111, frameon=False)
+
     def callback(weights):
         init_vx = np.reshape(weights[0:(rows*cols)], (rows, cols))
         init_vy = np.reshape(weights[(rows*cols):], (rows, cols))
         simulate(init_vx, init_vy, init_smoke, num_timesteps, ax)
 
     weights = fmin_cg(objective, init_dx_and_dy, fprime=grad_obj,
-                      maxiter=100, callback=callback)
+                      maxiter=num_solver_iters, callback=callback)
 
     init_vx = np.reshape(weights[0:(rows*cols)], (rows, cols))
     init_vy = np.reshape(weights[(rows*cols):], (rows, cols))
-    simulate(init_vx, init_vy, init_smoke, num_timesteps, ax)
+    simulate(init_vx, init_vy, init_smoke, num_timesteps, ax, render=True)
 
+    os.system("convert -delay 10 -loop 100 step*.png animated.gif")  # Convert using imagemagick.
+    os.system("rm step*.png")
