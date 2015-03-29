6d7b98a0f5bde954513db0a0073d059383bca475
Dougal <d.maclaurin@gmail.com>
2015-03-29 18:50:18 -0400

    Checked in script for inspecting numpy namespace

---
+++ scripts/numpy_namespace.py
@@ -0,0 +1,59 @@
+# Inspecting the numpy namespace and classifying numpy's functions
+import numpy as np
+from collections import defaultdict
+import types
+import inspect
+
+heading = lambda x : "-"*20 + str(x) + "-"*20
+
+np_types = defaultdict(list)
+for name, obj in np.__dict__.iteritems():
+    np_types[type(obj)].append(name)
+
+print "Objects in numpy namespace by type:"
+for t, vals in np_types.iteritems():
+    print heading(t)
+    print vals
+print "="*80
+
+all_ufuncs = np_types[np.ufunc]
+unary_ufuncs = []
+binary_ufuncs = []
+other_ufuncs = []
+for ufunc in all_ufuncs:
+    f = np.__dict__[ufunc]
+    if f.nin == 1:
+        unary_ufuncs.append(ufunc)
+    elif f.nin == 2:
+        binary_ufuncs.append(ufunc)
+    else:
+        other_ufuncs.append(ufunc)
+
+print heading("Unary ufuncs:")
+print sorted(unary_ufuncs)
+print heading("Binary ufuncs:")
+print sorted(binary_ufuncs)
+if other_ufuncs:
+    print heading("Other ufuncs:")
+    print sorted(other_ufuncs)
+
+all_regular_funcs = np_types[types.FunctionType] + np_types[types.BuiltinFunctionType]
+print heading("Stat functions with keepdims kwarg and ndarray method")
+keepdims_funcs = []
+all_other_funcs = []
+for func in all_regular_funcs:
+    try:
+        f = np.__dict__[func]
+        keepdims = "keepdims" in inspect.getargspec(f).args
+        axis = "axis" in inspect.getargspec(f).args
+        ndarray_method = hasattr(np.ndarray, func)
+        if keepdims and axis and ndarray_method:
+            keepdims_funcs.append(func)
+        else:
+            all_other_funcs.append(func)
+    except TypeError:
+        pass
+print sorted(keepdims_funcs)
+
+print heading("All other functions")
+print sorted(all_other_funcs)
