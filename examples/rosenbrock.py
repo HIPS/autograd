abbdb970e6ec0c471cb8e6d27d23552be149b1f6
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-03-30 13:42:22 -0400

    Added very simple rosenbrock demo

---
+++ examples/rosenbrock.py
@@ -0,0 +1,13 @@
+import autograd.numpy as np
+from autograd import grad
+from scipy.optimize import minimize
+
+def rosenbrock(x):
+    return 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
+
+# Build a function that also returns gradients, using autograd.
+rosenbrock_with_grad = grad(rosenbrock, return_function_value=True)
+
+# Optimize using BFGS.
+result = minimize(rosenbrock_with_grad, x0=[0.0, 0.0], jac=True)
+print "Found minimum at {0}".format(result.x)
