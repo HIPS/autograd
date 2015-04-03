32b050a46472cc007d7f724843dd45f0f5de2311
Dougal <d.maclaurin@gmail.com>
2015-04-03 16:08:20 -0400

    2D convolution. 2nd derivative w.r.t 2nd arg fails with mode='same'

--- autograd/scipy/signal.py
+++ autograd/scipy/signal.py
@@ -5,6 +5,11 @@ from autograd.core import getval, primitive
 from autograd.numpy import flipud
 
 convolve = primitive(scipy.signal.convolve)
+convolve2d = primitive(scipy.signal.convolve2d)
+
+def get_same_slice(L_in0, L_in1):
+    left_pad = L_in0 - (L_in1 + 1) / 2
+    return slice(left_pad, left_pad + L_in1)
 
 def make_grad_convolve_0(ans, in0, in1, mode='full'):
     if mode == 'full':
@@ -23,10 +28,8 @@ def make_grad_convolve_1(ans, in0, in1, mode='full'):
         return lambda g: flipud(convolve(flipud(g), in0, mode='valid'))
     elif mode == 'same':
         def grad_fun(g):
-            L_in0, L_in1 = in0.shape[0], in1.shape[0]
-            left_pad = L_in0 - (L_in1 + 1) / 2
-            idxs = slice(left_pad, left_pad + L_in1)
-            return flipud(convolve(in0, flipud(g), mode='full'))[idxs]
+            idxs = get_same_slice(in0.shape[0], in1.shape[0])
+            return  flipud(convolve(in0, flipud(g), mode='full'))[idxs]
         return grad_fun
     elif mode == 'valid':
         return lambda g: flipud(convolve(in0, flipud(g), mode='valid'))
@@ -34,3 +37,34 @@ def make_grad_convolve_1(ans, in0, in1, mode='full'):
         raise Exception("Unrecognized mode {0}".format(mode))
 
 convolve.defgrad(make_grad_convolve_1, argnum=1)
+
+def flip2d(x):
+    return x[::-1, ::-1]
+
+def make_grad_convolve2d_0(ans, in0, in1, mode='full'):
+    if mode == 'full':
+        return lambda g: convolve2d(g, flip2d(in1), mode='valid')
+    elif mode == 'same':
+        return lambda g: flip2d(convolve2d(flip2d(g), in1, mode='same'))
+    elif mode == 'valid':
+        return lambda g: convolve2d(g, flip2d(in1), mode='full')
+    else:
+        raise Exception("Unrecognized mode {0}".format(mode))
+
+convolve2d.defgrad(make_grad_convolve2d_0, argnum=0)
+
+def make_grad_convolve2d_1(ans, in0, in1, mode='full'):
+    if mode == 'full':
+        return lambda g: flip2d(convolve2d(flip2d(g), in0, mode='valid'))
+    elif mode == 'same':
+        def grad_fun(g):
+            idxs_0 = get_same_slice(in0.shape[0], in1.shape[0])
+            idxs_1 = get_same_slice(in0.shape[1], in1.shape[1])
+            return  flip2d(convolve(in0, flip2d(g), mode='full'))[idxs_0, idxs_1]
+        return grad_fun
+    elif mode == 'valid':
+        return lambda g: flip2d(convolve2d(in0, flip2d(g), mode='valid'))
+    else:
+        raise Exception("Unrecognized mode {0}".format(mode))
+
+convolve2d.defgrad(make_grad_convolve2d_1, argnum=1)
--- tests/test_scipy.py
+++ tests/test_scipy.py
@@ -11,4 +11,8 @@ def test_convolve(): combo_check(autograd.scipy.signal.convolve, [0,1],
                                     [R(2), R(3), R(4)],
                                     mode=['full', 'valid', 'same'])
 
-test_convolve()
+def test_convolve2d(): combo_check(autograd.scipy.signal.convolve2d, [0, 1],
+                                   [R(4, 3), R(5, 4), R(6, 7)],
+                                   [R(2, 2), R(3, 2), R(4, 3)],
+                                   mode=['full', 'valid', 'same'])
+
