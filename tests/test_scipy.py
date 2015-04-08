78f9960c5f21a7b6fa773ecf603f183ccb0d4eb8
Dougal <d.maclaurin@gmail.com>
2015-04-07 23:21:53 -0400

    Generalized convolution to allow conv and dot axes to be specified and other axes ignored.

--- tests/test_scipy.py
+++ tests/test_scipy.py
@@ -58,12 +58,12 @@ def test_logsumexp5(): combo_check(autograd.scipy.misc.logsumexp, [0],
 def test_convolve(): combo_check(autograd.scipy.signal.convolve, [0,1],
                                  [R(4), R(5), R(6)],
                                  [R(2), R(3), R(4)],
-                                 mode=['full', 'valid', 'same'])
+                                 mode=['full', 'valid'])
 
-def test_convolve2d(): combo_check(autograd.scipy.signal.convolve2d, [0, 1],
+def test_convolve_2d(): combo_check(autograd.scipy.signal.convolve, [0, 1],
                                    [R(4, 3), R(5, 4), R(6, 7)],
                                    [R(2, 2), R(3, 2), R(4, 3)],
-                                   mode=['full', 'valid', 'same'])
+                                   mode=['full', 'valid'])
 
 
 ### Special ###
