670cec8c9bad0cb45803c72e74e3a914e976de97
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-04-10 11:32:57 -0400

    Fixed setup.py to include all packages

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='autograd',
       description='Autograd: Computes derivatives of complicated numpy code.',
       author='Dougal Maclaurin and David Duvenaud',
       author_email = "maclaurin@physics.harvard.edu, dduvenaud@seas.harvard.edu",
-      packages=['autograd'],
+      packages=['autograd', 'autograd.numpy', 'autograd.scipy', 'autograd.scipy.stats'],
       long_description="Stateless reverse-mode autodiff implementation that also offers higher-order derivatives.")
