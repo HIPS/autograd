0a3fd980e60fa9ff233cbbe25f6a5c4968567d9d
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-04-12 11:33:26 -0400

    Made gradient-defining example a little nicer

--- examples/define_gradient.py
+++ examples/define_gradient.py
@@ -8,37 +8,37 @@ from autograd.core import grad, primitive
 from autograd.util import quick_grad_check
 
 
-# The @primitive decorator tells autograd not to look inside this function,
-# but instead to treat it as a black box, whose gradient might be specified
-# later.  Functions that have this decorator can contain anything that Python
-# knows how to execute.
+# @primitive tells autograd not to look inside this function, but instead
+# to treat it as a black box, whose gradient might be specified# later.
+# Functions with this decorator can contain anything that Python knows
+# how to execute.
 @primitive
-def logsumexp(x):                     # also defined in scipy.misc
-    return np.log(np.sum(np.exp(x)))
+def logsumexp(x):
+    """Numerically stable log(sum(exp(x))), also defined in scipy.misc"""
+    max_x = np.max(x)
+    return max_x + np.log(np.sum(np.exp(x - max_x)))
 
-# This function will get called on the forward pass,
-# and tells autograd how to compute the gradient on the reverse pass.
-# For the purposes of computing second derivatives, all the code inside
-# the gradient-making function must consist of functions known to autograd.
+# Next, we write a function that specifies the gradient with a closure.
+# The reason for the closure is so that the gradient can depend
+# on both the input to the original function (x), and the output of the
+# original function (ans).
 def make_grad_logsumexp(ans, x):
-    # ans is the result of the forward computation, which we can re-use
-    # in our gradient computation.
+    # If you want to be able to take higher-order derivatives, then all the
+    # code inside this function must be itself differentiable by autograd.
     def gradient_product(g):
-        # Because autograd uses reverse-mode differentiation, g always contains
-        #  the gradient of the objective w.r.t. the output of logsumexp.
-        # This function multiplies g with the Jacobian of logsumexp.
+        # This closure multiplies g with the Jacobian of logsumexp (d_ans/d_x).
+        # Because autograd uses reverse-mode differentiation, g contains
+        # the gradient of the objective w.r.t. ans, the output of logsumexp.
         return np.full(x.shape, g) * np.exp(x - np.full(x.shape, ans))
     return gradient_product
 
-# Now, the only thing left to do is to tell autograd that
-# logsumexmp has a gradient-making function:
+# Now we tell autograd that logsumexmp has a gradient-making function.
 logsumexp.defgrad(make_grad_logsumexp)
 
 
 if __name__ == '__main__':
     # Now we can use logsumexp() inside a larger function that we want
     # to differentiate.
-
     def example_func(y):
         z = y**2
         lse = logsumexp(z)
@@ -48,4 +48,4 @@ if __name__ == '__main__':
     print "Gradient: ", grad_of_example(npr.randn(10))
 
     # Check the gradients numerically, just to be safe.
-    quick_grad_check(example_func,npr.randn(10))
+    quick_grad_check(example_func, npr.randn(10))
