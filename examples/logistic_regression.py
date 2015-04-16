e8843cbdb4c3b3fdd7121c5b90da276d7ffe657b
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-04-16 12:39:34 -0400

    Made logistic regression example smaller and clearer.

--- examples/logistic_regression.py
+++ examples/logistic_regression.py
@@ -3,13 +3,35 @@ from autograd import grad
 from autograd.util import quick_grad_check
 
 def sigmoid(x):
+    return 0.5*(np.tanh(x) + 1)
+
 def logistic_predictions(weights, inputs):
     # Outputs probability of a label being true according to logistic model.
     return sigmoid(np.dot(inputs, weights))
 
 def training_loss(weights):
+    # Training loss is the negative log-likelihood of the training labels.
+    preds = logistic_predictions(weights, inputs)
+    label_probabilities = preds * targets + (1 - preds) * (1 - targets)
+    return -np.sum(np.log(label_probabilities))
 
+# Build a toy dataset.
+inputs = np.array([[0.52, 1.12,  0.77],
+                   [0.88, -1.08, 0.15],
+                   [0.52, 0.06, -1.30],
+                   [0.74, -2.49, 1.39]])
+targets = np.array([True, True, False, True])
 
+# Build a function that returns gradients of training loss using autograd.
+training_gradient_fun = grad(training_loss)
 
 # Check the gradients numerically, just to be safe.
+weights = np.array([0.0, 0.0, 0.0])
+quick_grad_check(training_loss, weights)
 
+# Optimize weights using gradient descent.
+print "Initial loss:", training_loss(weights)
+for i in xrange(100):
+    weights -= training_gradient_fun(weights) * 0.01
+
+print  "Trained loss:", training_loss(weights)
