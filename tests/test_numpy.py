aa54fc955667812593346ecafa6298aa65dd0d66
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-05-10 11:46:30 -0400

    Minor cleanup of improved trace() grad

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -349,6 +349,23 @@ def test_trace():
     check_grads(fun, mat)
     check_grads(d_fun, mat)
 
+def test_trace2():
+    def fun(x): return np.trace(x, offset=offset)
+    d_fun = lambda x : to_scalar(grad(fun)(x))
+    mat = npr.randn(11, 10)
+    offset = npr.randint(-9,11)
+    check_grads(fun, mat)
+    check_grads(d_fun, mat)
+
+# TODO: Allow extra dimensions and dim1, dim2 args.
+# def test_trace_extradims():
+#     def fun(x): return np.trace(x, offset=offset)
+#     d_fun = lambda x : to_scalar(grad(fun)(x))
+#     mat = npr.randn(5, 6, 4)
+#     offset = npr.randint(-5,6)
+#     check_grads(fun, mat)
+#     check_grads(d_fun, mat)
+
 def test_diag():
     def fun(x): return to_scalar(np.diag(x))
     d_fun = lambda x : to_scalar(grad(fun)(x))
