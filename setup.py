057d4d3725fab78013ae3074bdcb366ee80939e5
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2015-05-13 15:01:05 -0400

    Note compatibility with Python 3.4.

--- setup.py
+++ setup.py
@@ -14,4 +14,5 @@ setup(name='autograd',
       license='MIT',
       classifiers=['Development Status :: 4 - Beta',
                    'License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python :: 2.7'])
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4'])
