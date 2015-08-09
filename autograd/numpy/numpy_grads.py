Fixed bug in gradient of np.repeat()

autograd/numpy/numpy_grads.py
@@ -124,7 +124,6 @@ anp.roll.defgrad(   lambda ans, x, shift, axis=None  : lambda g : anp.roll(g, -s
 anp.ravel.defgrad(  lambda ans, x, order=None   : lambda g : anp.reshape(g, anp.shape(x), order=order))
 anp.expand_dims.defgrad(lambda ans, x, axis     : lambda g : anp.reshape(g, anp.shape(x)))
 anp.squeeze.defgrad(lambda ans, x, axis=None    : lambda g : anp.reshape(g, anp.shape(x)))
-anp.repeat.defgrad( lambda ans, x, shape, axis  : lambda g : anp.sum(g, axis, keepdims=True))
 anp.split.defgrad(  lambda ans, x, idxs, axis=0 : lambda g : anp.concatenate(g, axis=axis))
 anp.diag.defgrad(   lambda ans, x, k=0          : lambda g : anp.diag(g, k))
 anp.flipud.defgrad( lambda ans, x,              : lambda g : anp.flipud(g))
@@ -145,6 +144,23 @@ anp.where.defgrad( lambda ans, c, x=None, y=None : lambda g : anp.where(c, g, an
 anp.where.defgrad( lambda ans, c, x=None, y=None : lambda g : anp.where(c, anp.zeros(g.shape), g), argnum=2)
 
 # ----- Trickier grads -----
+
+def make_grad_reshape(ans, x, repeats, axis=None):
+    shape = x.shape
+    if axis is None:  # If axis is none, np.repeat() repeats the flattened array.
+        def reshape_grad(g):
+            expanded = anp.reshape(g, (anp.prod(shape),) + (repeats,))
+            return anp.reshape(anp.sum(expanded, axis=1, keepdims=False), shape)
+        return reshape_grad
+    else:
+        if shape[axis] == 1:  # For this common case, the logic is simple.
+            return lambda g: anp.sum(g, axis=axis, keepdims=True)
+        else:
+            def reshape_grad(g):
+                expanded = anp.reshape(g, shape[0:axis+1] + (repeats,) + shape[axis+1:])
+                return anp.sum(expanded, axis=axis+1, keepdims=False)
+            return reshape_grad
+anp.repeat.defgrad(make_grad_reshape)
 
 isarray = lambda x : isinstance(getval(x), anp.ndarray)
 def make_grad_transpose(ans, x, axes=None):

tests/test_numpy.py
@@ -658,10 +658,37 @@ def test_squeeze_method():
     check_grads(fun, A)
     check_grads(d_fun, A)
 
-
 def test_repeat():
-    A = npr.randn(5, 1, 4)
+    A = npr.randn(5, 3, 4)
     def fun(x): return to_scalar(np.repeat(x, 2, axis=1))
+    d_fun = lambda x : to_scalar(grad(fun)(x))
+    check_grads(fun, A)
+    check_grads(d_fun, A)
+
+def test_repeat_axis1_rep1():
+    A = npr.randn(5, 3, 4)
+    def fun(x): return to_scalar(np.repeat(x, 1, axis=1))
+    d_fun = lambda x : to_scalar(grad(fun)(x))
+    check_grads(fun, A)
+    check_grads(d_fun, A)
+
+def test_repeat_axis0():
+    A = npr.randn(5, 3)
+    def fun(x): return to_scalar(np.repeat(x, 2, axis=0))
+    d_fun = lambda x : to_scalar(grad(fun)(x))
+    check_grads(fun, A)
+    check_grads(d_fun, A)
+
+def test_repeat_1d_axis0():
+    A = npr.randn(5)
+    def fun(x): return to_scalar(np.repeat(x, 2, axis=0))
+    d_fun = lambda x : to_scalar(grad(fun)(x))
+    check_grads(fun, A)
+    check_grads(d_fun, A)
+
+def test_repeat_axis0_rep1():
+    A = npr.randn(5, 1)
+    def fun(x): return to_scalar(np.repeat(x, 1, axis=0))
     d_fun = lambda x : to_scalar(grad(fun)(x))
     check_grads(fun, A)
     check_grads(d_fun, A)

tests/test_systematic.py
@@ -78,13 +78,14 @@ def test_op_pow(): binary_ufunc_check(op.pow, lims_A=[0.7, 2.0])
 
 # Misc tests
 
-def test_transpose(): combo_check(np.transpose, [0],
-                                  [npr.randn(2, 3, 4)],
-                                  axes = [None, [0, 1, 2], [0, 2, 1],
-                                                [2, 0, 1], [2, 1, 0],
-                                                [1, 0, 2], [1, 2, 0]])
-
 R = npr.randn
+def test_transpose(): combo_check(np.transpose, [0],
+                                  [R(2, 3, 4)], axes = [None, [0, 1, 2], [0, 2, 1],
+                                                              [2, 0, 1], [2, 1, 0],
+                                                              [1, 0, 2], [1, 2, 0]])
+def test_repeat(): combo_check(np.repeat, [0], [R(2, 3, 4), R(3, 1)],
+                               repeats=[0,1,2], axis = [None, 0, 1])
+
 def test_dot(): combo_check(np.dot, [0, 1],
                             [1.5, R(3), R(2, 3)],
                             [0.3, R(3), R(3, 4)])