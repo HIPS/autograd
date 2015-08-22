b86c37addd03fff4711b6b1bf8d77a0b0e85882f
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-08-22 14:53:40 -0400

    Can now take gradient w.r.t. dicts

--- autograd/container_types.py
+++ autograd/container_types.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from autograd.core import primitive, Node, getval, zeros_like, cast
 from six.moves import zip
+import six
 
 class TupleNode(Node):
     __slots__ = []
@@ -83,3 +84,54 @@ def list_untake(x, idx, template):
     return result
 list_untake.defgrad(lambda ans, x, idx, template : lambda g : list_take(g, idx))
 list_untake.defgrad_is_zero(argnums=(1, 2))
+
+
+class DictNode(Node):
+    __slots__ = []
+    def __getitem__(self, idx):
+        return dict_take(self, idx)
+    def __len__(self):
+        return len(self.value)
+    def __iter__(self):
+        return self.value.__iter__()
+
+    @staticmethod
+    def zeros_like(self):
+        return {k : zeros_like(v) for k, v in six.iteritems(getval(self))}
+
+    @staticmethod
+    def sum_outgrads(outgrads):
+        return primitive_sum_dicts(*outgrads)
+
+    @staticmethod
+    def cast(value, example):
+        return cast(value, cast_to_dict)
+
+def cast_to_dict(x):
+    return dict(x)
+
+Node.type_mappings[dict] = DictNode
+
+@primitive
+def primitive_sum_dicts(*dicts):
+    """Takes a list of dicts having identical keys.
+       Returns a new dict whose values are the sum over all input dicts."""
+    # assert set(dicts[0]) == set(dicts[0]).intersection(*dicts)
+    keys = dicts[0]
+    return {k : sum([dict[k] for dict in dicts]) for k in keys}
+primitive_sum_dicts.gradmaker = lambda *args : lambda g : g
+
+@primitive
+def dict_take(A, idx):
+    return A[idx]
+def make_grad_dict_take(ans, A, idx):
+    return lambda g : dict_untake(g, idx, A)
+dict_take.defgrad(make_grad_dict_take)
+
+@primitive
+def dict_untake(x, idx, template):
+    result = dict(zeros_like(template))
+    result[idx] = x
+    return result
+dict_untake.defgrad(lambda ans, x, idx, template : lambda g : dict_take(g, idx))
+dict_untake.defgrad_is_zero(argnums=(1, 2))
--- tests/test_dict.py
+++ tests/test_dict.py
@@ -1,63 +1,63 @@
-# import autograd.numpy as np
-# import autograd.numpy.random as npr
-# from test_util import *
-# from autograd import grad
-# npr.seed(1)
+import autograd.numpy as np
+import autograd.numpy.random as npr
+from autograd.util import check_grads
+from autograd import grad
 
-# def test_getter():
-#     def fun(input_dict):
-#         A = np.sum(input_dict['item_1'])
-#         B = np.sum(input_dict['item_2'])
-#         C = np.sum(input_dict['item_2'])
-#         return A + B + C
 
-#     d_fun = grad(fun)
-#     input_dict = {'item_1' : npr.randn(5, 6),
-#                   'item_2' : npr.randn(4, 3),
-#                   'item_X' : npr.randn(2, 4)}
+def test_getter():
+    def fun(input_dict):
+        A = np.sum(input_dict['item_1'])
+        B = np.sum(input_dict['item_2'])
+        C = np.sum(input_dict['item_2'])
+        return A + B + C
 
-#     result = d_fun(input_dict)
-#     assert np.allclose(result['item_1'], np.ones((5, 6)))
-#     assert np.allclose(result['item_2'], 2 * np.ones((4, 3)))
-#     assert np.allclose(result['item_X'], np.zeros((2, 4)))
+    d_fun = grad(fun)
+    input_dict = {'item_1' : npr.randn(5, 6),
+                  'item_2' : npr.randn(4, 3),
+                  'item_X' : npr.randn(2, 4)}
 
-# def test_grads():
-#     def fun(input_dict):
-#         A = np.sum(np.sin(input_dict['item_1']))
-#         B = np.sum(np.cos(input_dict['item_2']))
-#         return A + B
+    result = d_fun(input_dict)
+    assert np.allclose(result['item_1'], np.ones((5, 6)))
+    assert np.allclose(result['item_2'], 2 * np.ones((4, 3)))
+    assert np.allclose(result['item_X'], np.zeros((2, 4)))
 
-#     def d_fun(input_dict):
-#         g = grad(fun)(input_dict)
-#         A = np.sum(g['item_1'])
-#         B = np.sum(np.sin(g['item_1']))
-#         C = np.sum(np.sin(g['item_2']))
-#         return A + B + C
+def test_grads():
+    def fun(input_dict):
+        A = np.sum(np.sin(input_dict['item_1']))
+        B = np.sum(np.cos(input_dict['item_2']))
+        return A + B
 
-#     input_dict = {'item_1' : npr.randn(5, 6),
-#                   'item_2' : npr.randn(4, 3),
-#                   'item_X' : npr.randn(2, 4)}
+    def d_fun(input_dict):
+        g = grad(fun)(input_dict)
+        A = np.sum(g['item_1'])
+        B = np.sum(np.sin(g['item_1']))
+        C = np.sum(np.sin(g['item_2']))
+        return A + B + C
 
-#     check_grads(fun, input_dict)
-#     check_grads(d_fun, input_dict)
+    input_dict = {'item_1' : npr.randn(5, 6),
+                  'item_2' : npr.randn(4, 3),
+                  'item_X' : npr.randn(2, 4)}
 
-# def test_iter():
-#     def fun(input_dict):
-#         for i, k in enumerate(input_dict):
-#             A = np.sum(np.sin(input_dict[k])) * (i + 1.0)
-#             B = np.sum(np.cos(input_dict[k]))
-#         return A + B
+    check_grads(fun, input_dict)
+    check_grads(d_fun, input_dict)
 
-#     def d_fun(input_dict):
-#         g = grad(fun)(input_dict)
-#         A = np.sum(g['item_1'])
-#         B = np.sum(np.sin(g['item_1']))
-#         C = np.sum(np.sin(g['item_2']))
-#         return A + B + C
+def test_iter():
+    def fun(input_dict):
+        for i, k in enumerate(input_dict):
+            A = np.sum(np.sin(input_dict[k])) * (i + 1.0)
+            B = np.sum(np.cos(input_dict[k]))
+        return A + B
 
-#     input_dict = {'item_1' : npr.randn(5, 6),
-#                   'item_2' : npr.randn(4, 3),
-#                   'item_X' : npr.randn(2, 4)}
+    def d_fun(input_dict):
+        g = grad(fun)(input_dict)
+        A = np.sum(g['item_1'])
+        B = np.sum(np.sin(g['item_1']))
+        C = np.sum(np.sin(g['item_2']))
+        return A + B + C
 
-#     check_grads(fun, input_dict)
-#     check_grads(d_fun, input_dict)
+    input_dict = {'item_1' : npr.randn(5, 6),
+                  'item_2' : npr.randn(4, 3),
+                  'item_X' : npr.randn(2, 4)}
+
+    check_grads(fun, input_dict)
+    check_grads(d_fun, input_dict)
