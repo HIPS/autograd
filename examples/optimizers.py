3d1745108bd912723047a1f7fedeab7319260bdb
David Duvenaud <dduvenaud@seas.harvard.edu>
2015-10-22 21:45:18 -0400

    Updated adam params to match meaning in paper

--- examples/optimizers.py
+++ examples/optimizers.py
@@ -31,7 +31,7 @@ def rmsprop(grad, x, callback=None, num_iters=100, step_size=0.1, gamma=0.9, eps
     return x
 
 def adam(grad, x, callback=None, num_iters=100,
-         step_size=0.001, b1=0.1, b2=0.0001, eps = 10**-8):
+         step_size=0.001, b1=0.9, b2=0.999, eps=10**-8):
     """Adam as described in http://arxiv.org/pdf/1412.6980.pdf.
     It's basically RMSprop with momentum and some correction terms."""
     m = np.zeros(len(x))
@@ -39,9 +39,9 @@ def adam(grad, x, callback=None, num_iters=100,
     for i in range(num_iters):
         g = grad(x, i)
         if callback: callback(x, i, g)
-        m = b1 * g      + (1 - b1) * m     # First  moment estimate.
-        v = b2 * (g**2) + (1 - b2) * v      # Second moment estimate.
-        mhat = m / (1 - (1 - b1)**(i + 1))  # Bias correction.
-        vhat = v / (1 - (1 - b2)**(i + 1))
+        m = (1 - b1) * g      + b1 * m  # First  moment estimate.
+        v = (1 - b2) * (g**2) + b2 * v  # Second moment estimate.
+        mhat = m / (1 - b1**(i + 1))    # Bias correction.
+        vhat = v / (1 - b2**(i + 1))
         x -= step_size*mhat/(np.sqrt(vhat) + eps)
     return x
