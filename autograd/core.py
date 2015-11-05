e3f63bb18719bab2299809be7b42d540bf15fa01
Matthew Johnson <mattjj@csail.mit.edu>
2015-11-04 20:17:38 -0500

    make improve_exception_message function

--- autograd/core.py
+++ autograd/core.py
@@ -60,10 +60,8 @@ def forward_pass(fun, args, kwargs, argnum=0):
         args[argnum] = merge_tapes(start_node, arg_wrt)
         try:
             end_node = fun(*args, **kwargs)
-        except TypeError as e:
-            if e.message == 'float() argument must be a string or a number':
-                raise TypeError(e.message + "\n** autograd doesn't support assigning into arrays **")
-            raise e
+        except Exception as e:
+            raise improve_exception_message(e)
         return start_node, end_node, tape
 
 def backward_pass(start_node, end_node, tape):
@@ -109,6 +107,18 @@ def attach_name_and_doc(fun, argnum, opname):
         finally:
             return gradfun
     return wrap
+
+
+def improve_exception_message(e):
+    common_errors = {
+        (TypeError, 'float() argument must be a string or a number'):
+            "** autograd doesn't support assigning into arrays **",
+    }
+
+    keypair = (type(e), e.message)
+    if keypair in common_errors:
+        return type(e)(e.message + '\n' + common_errors[keypair])
+    return e
 
 def cast_to_node_type(x, node_type, example):
     if type(new_node(getval(x))) is not node_type:
