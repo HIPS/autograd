fb19f5c8302861110b14ac0845ea87901f159404
Matthew Johnson <mattjj@csail.mit.edu>
2015-11-07 11:36:30 -0500

    add an sdist command to setup that tries to generate extension modules

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
 from distutils.errors import CompileError
 from warnings import warn
 import os
@@ -29,6 +30,18 @@ class build_ext(_build_ext):
         except CompileError:
             warn('Failed to compile optional extension modules')
 
+# when building a source distribution (only for autograd maintainers), wrap the
+# sdist command to try to generate extension module sources
+class sdist(_sdist):
+    def run(self):
+        try:
+            from Cython.Build import cythonize
+            cythonize(os.path.join('**','*.pyx'))
+        except:
+            warn('Failed to generate extension files from Cython sources')
+        finally:
+            _sdist.run(self)
+
 # list the extension files to build
 extensions = [
     Extension(
@@ -40,13 +53,13 @@ extensions = [
 if use_cython:
     from Cython.Build import cythonize
     try:
-        extensions = cythonize('**/*.pyx')
+        extensions = cythonize(os.path.join('**','*.pyx'))
     except:
         warn('Failed to generate extension module code from Cython files')
 
 setup(
     name='autograd',
-    version='1.1.1',
+    version='1.1.2',
     description='Efficiently computes derivatives of numpy code.',
     author='Dougal Maclaurin and David Duvenaud and Matthew Johnson',
     author_email="maclaurin@physics.harvard.edu, dduvenaud@seas.harvard.edu, mattjj@csail.mit.edu",
@@ -63,5 +76,5 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.4'],
     ext_modules=extensions,
-    cmdclass={'build_ext': build_ext},
+    cmdclass={'build_ext': build_ext, 'sdist': sdist},
 )
