6157867c9a5e58f4be8d2f0c905bd4a7f10dd4ff
Matthew Johnson <mattjj@csail.mit.edu>
2015-11-08 11:10:40 -0500

    reorder convenience_wrappers so the most popular ones are at the top

--- autograd/convenience_wrappers.py
+++ autograd/convenience_wrappers.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 import autograd.numpy as np
 from autograd.core import grad, getval, jacobian
 from collections import OrderedDict
-from future.utils import iteritems
 
 
 def multigrad(fun, argnums=[0]):
@@ -18,6 +17,57 @@ def multigrad(fun, argnums=[0]):
         multi_arg = tuple([args[i] for i in argnums])
         return gradfun(multi_arg, *args, **kwargs)
     return gradfun_rearranged
+
+def elementwise_grad(fun, argnum=0):
+    """Like `jacobian`, but produces a function which computes just the diagonal
+    of the Jacobian, and does the computation in one pass rather than in a loop.
+    Note: this is only valid if the Jacobian is diagonal. Only arrays are
+    currently supported. Can be used for broadcasting."""
+    def sum_output(*args, **kwargs):
+        return np.sum(fun(*args, **kwargs))
+    return grad(sum_output, argnum=argnum)
+
+def hessian(fun, argnum=0):
+    "Returns a function that computes the exact Hessian."
+    return jacobian(jacobian(fun, argnum), argnum)
+
+def hessian_vector_product(fun, argnum=0):
+    """Builds a function that returns the exact Hessian-vector product.
+    The returned function has arguments (*args, vector, **kwargs), and takes
+    roughly 4x as long to evaluate as the original function."""
+    fun_grad = grad(fun, argnum)
+    def vector_dot_grad(*args, **kwargs):
+        args, vector = args[:-1], args[-1]
+        return np.dot(vector, fun_grad(*args, **kwargs))
+    return grad(vector_dot_grad, argnum)  # Grad wrt original input.
+
+def value_and_grad(fun, argnum=0):
+    """Returns a function that returns both value and gradient. Suitable for use
+    in scipy.optimize"""
+    def double_val_fun(*args, **kwargs):
+        val = fun(*args, **kwargs)
+        return val, getval(val)
+    gradval_and_val = grad_and_aux(double_val_fun, argnum)
+
+    def value_and_grad_fun(*args, **kwargs):
+        gradval, val = gradval_and_val(*args, **kwargs)
+        return val, gradval
+
+    return value_and_grad_fun
+
+def grad_and_aux(fun, argnum=0):
+    """Builds a function that returns the gradient of the first output and the
+    (unmodified) second output of a function that returns two outputs."""
+    def grad_and_aux_fun(*args, **kwargs):
+        saved_aux = []
+        def return_val_save_aux(*args, **kwargs):
+            val, aux = fun(*args, **kwargs)
+            saved_aux.append(aux)
+            return val
+        gradval = grad(return_val_save_aux, argnum)(*args, **kwargs)
+        return gradval, saved_aux[0]
+
+    return grad_and_aux_fun
 
 def multigrad_dict(fun):
     "Takes gradients wrt all arguments simultaneously,"
@@ -61,54 +111,3 @@ def multigrad_dict(fun):
         return OrderedDict((argname, grad_dict[argname]) for argname in argdict)
 
     return gradfun
-
-def grad_and_aux(fun, argnum=0):
-    """Builds a function that returns the gradient of the first output and the
-    (unmodified) second output of a function that returns two outputs."""
-    def grad_and_aux_fun(*args, **kwargs):
-        saved_aux = []
-        def return_val_save_aux(*args, **kwargs):
-            val, aux = fun(*args, **kwargs)
-            saved_aux.append(aux)
-            return val
-        gradval = grad(return_val_save_aux, argnum)(*args, **kwargs)
-        return gradval, saved_aux[0]
-
-    return grad_and_aux_fun
-
-def value_and_grad(fun, argnum=0):
-    """Returns a function that returns both value and gradient. Suitable for use
-    in scipy.optimize"""
-    def double_val_fun(*args, **kwargs):
-        val = fun(*args, **kwargs)
-        return val, getval(val)
-    gradval_and_val = grad_and_aux(double_val_fun, argnum)
-
-    def value_and_grad_fun(*args, **kwargs):
-        gradval, val = gradval_and_val(*args, **kwargs)
-        return val, gradval
-
-    return value_and_grad_fun
-
-def elementwise_grad(fun, argnum=0):
-    """Like `jacobian`, but produces a function which computes just the diagonal
-    of the Jacobian, and does the computation in one pass rather than in a loop.
-    Note: this is only valid if the Jacobian is diagonal. Only arrays are
-    currently supported."""
-    def sum_output(*args, **kwargs):
-        return np.sum(fun(*args, **kwargs))
-    return grad(sum_output, argnum=argnum)
-
-def hessian_vector_product(fun, argnum=0):
-    """Builds a function that returns the exact Hessian-vector product.
-    The returned function has arguments (*args, vector, **kwargs), and takes
-    roughly 4x as long to evaluate as the original function."""
-    fun_grad = grad(fun, argnum)
-    def vector_dot_grad(*args, **kwargs):
-        args, vector = args[:-1], args[-1]
-        return np.dot(vector, fun_grad(*args, **kwargs))
-    return grad(vector_dot_grad, argnum)  # Grad wrt original input.
-
-def hessian(fun, argnum=0):
-    "Returns a function that computes the exact Hessian."
-    return jacobian(jacobian(fun, argnum), argnum)
