f93d48735b8d4a27fa4f3f79a329ec9c7b6d750d
David Duvenaud <dduvenaud@seas.harvard.edu>
2016-04-11 21:11:26 -0400

    Made gmm example way smaller

--- examples/data.py
+++ examples/data.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import autograd.numpy as np
+import autograd.numpy.random as npr
 import imp, urllib
 
 def load_mnist():
@@ -17,3 +18,18 @@ def load_mnist():
 
     return N_data, train_images, train_labels, test_images, test_labels
 
+
+def make_pinwheel(radial_std, tangential_std, num_classes, num_per_class, rate):
+    """Based on code by Ryan P. Adams."""
+    rads = np.linspace(0, 2*np.pi, num_classes, endpoint=False)
+
+    features = npr.randn(num_classes*num_per_class, 2) \
+        * np.array([radial_std, tangential_std])
+    features[:,0] += 1
+    labels = np.repeat(np.arange(num_classes), num_per_class)
+
+    angles = rads[labels] + rate * np.exp(features[:,0])
+    rotations = np.stack([np.cos(angles), -np.sin(angles), np.sin(angles), np.cos(angles)])
+    rotations = np.reshape(rotations.T, (-1, 2, 2))
+
+    return np.einsum('ti,tij->tj', features, rotations)
--- examples/gmm.py
+++ examples/gmm.py
@@ -12,92 +12,65 @@ from autograd import value_and_grad
 from scipy.optimize import minimize
 from autograd.scipy.misc import logsumexp
 import autograd.scipy.stats.multivariate_normal as mvn
+from data import make_pinwheel
+from autograd.util import flatten_func
 
 
-class WeightsParser(object):
-    """A helper class to index into a parameter vector."""
-    def __init__(self):
-        self.idxs_and_shapes = {}
-        self.num_weights = 0
+def init_gmm_params(num_components, D, scale, rs=npr.RandomState(0)):
+    return {'log proportions' : rs.randn(num_components) * scale,
+            'means' :           rs.randn(num_components, D) * scale,
+            'lower triangles' : rs.randn(num_components, D, D) * scale,
+            'log diagonals' :   rs.randn(num_components, D) * scale}
 
-    def add_shape(self, name, shape):
-        start = self.num_weights
-        self.num_weights += np.prod(shape)
-        self.idxs_and_shapes[name] = (slice(start, self.num_weights), shape)
+def log_normalize(x):
+    return x - logsumexp(x)
 
-    def get(self, vect, name):
-        idxs, shape = self.idxs_and_shapes[name]
-        return np.reshape(vect[idxs], shape)
+def unpack_gmm_params(params):
+    """Unpacks parameter vector into the proportions, means and covariances
+    of each mixture component.  The covariance matrices are parametrized by
+    their Cholesky decompositions."""
+    normalized_log_proportions = log_normalize(params['log proportions'])
+    lower_tris = np.tril(params['lower triangles'], k=-1)
+    diag_chols = np.exp(params['log diagonals'])
+    chols = lower_tris + np.make_diagonal(diag_chols, axis1=-1, axis2=-2)
+    return normalized_log_proportions, params['means'], chols
 
+def gmm_log_likelihood(params, data):
+    cluster_lls = []
+    D = data.shape[1]
+    for log_proportion, mean, chol in zip(*unpack_gmm_params(params)):
+        cov = np.dot(chol.T, chol) + 0.000001 * np.eye(D)
+        cluster_lls.append(log_proportion + mvn.logpdf(data, mean, cov))
+    return np.sum(logsumexp(np.vstack(cluster_lls), axis=0))
 
-def make_gmm_funcs(num_components, D):
-
-    parser = WeightsParser()
-    parser.add_shape('log proportions',  num_components)
-    parser.add_shape('means',           (num_components, D))
-    parser.add_shape('lower triangles', (num_components, D, D))
-    parser.add_shape('log diagonals',   (num_components, D))
-
-    def unpack_params(params):
-        """Unpacks parameter vector into the proportions, means and covariances
-        of each mixture component.  The covariance matrices are parametrized by
-        their Cholesky decompositions."""
-        log_proportions    = parser.get(params, 'log proportions')
-        normalized_log_proportions = log_proportions - logsumexp(log_proportions)
-        means              = parser.get(params, 'means')
-        lower_tris = np.tril(parser.get(params, 'lower triangles'), k=-1)
-        diag_chols = np.exp( parser.get(params, 'log diagonals'))
-        chols = lower_tris + np.make_diagonal(diag_chols, axis1=-1, axis2=-2)
-        return normalized_log_proportions, means, chols
-
-    def log_marginal_likelihood(params, data):
-        cluster_lls = []
-        for log_proportion, mean, chol in zip(*unpack_params(params)):
-            cov = np.dot(chol.T, chol) + 0.000001 * np.eye(D)
-            cluster_log_likelihood = log_proportion + mvn.logpdf(data, mean, cov)
-            cluster_lls.append(np.expand_dims(cluster_log_likelihood, axis=0))
-        cluster_lls = np.concatenate(cluster_lls, axis=0)
-        return np.sum(logsumexp(cluster_lls, axis=0))
-
-    return parser.num_weights, log_marginal_likelihood, unpack_params
-
-
-def make_pinwheel_data(num_spokes=5, points_per_spoke=40, rate=1.0, noise_std=0.005):
-    """Make synthetic data in the shape of a pinwheel."""
-    spoke_angles = np.linspace(0, 2 * np.pi, num_spokes + 1)[:-1]
-    rs = npr.RandomState(0)
-    x = np.linspace(0.1, 1, points_per_spoke)
-    xs = np.concatenate([x * np.cos(angle + x * rate) + noise_std * rs.randn(len(x))
-                         for angle in spoke_angles])
-    ys = np.concatenate([x * np.sin(angle + x * rate) + noise_std * rs.randn(len(x))
-                         for angle in spoke_angles])
-    return np.concatenate([np.expand_dims(xs, 1), np.expand_dims(ys,1)], axis=1)
+def plot_gmm(params, ax, num_points=100):
+    angles = np.expand_dims(np.linspace(0, 2*np.pi, num_points), 1)
+    circle_pts = np.hstack([np.cos(angles), np.sin(angles)]) * 2.0
+    for log_proportion, mean, chol in zip(*unpack_gmm_params(params)):
+        cur_pts = mean + np.dot(circle_pts, chol)
+        alpha = np.minimum(1.0, np.exp(log_proportion) * 10)
+        ax.plot(cur_pts[:, 0], cur_pts[:, 1], '-', alpha=alpha)
 
 
 if __name__ == '__main__':
 
-    num_guassians = 10
-    num_gmm_params, log_marginal_likelihood, unpack_params = \
-        make_gmm_funcs(num_components=num_guassians, D=2)
+    init_params = init_gmm_params(num_components=10, D=2, scale=0.1)
 
-    data = make_pinwheel_data()
+    data = make_pinwheel(radial_std=0.3, tangential_std=0.1, num_classes=3,
+                         num_per_class=100, rate=0.4)
+
     def objective(params):
-        return -log_marginal_likelihood(params, data)
+        return -gmm_log_likelihood(params, data)
 
-    def plot_gmm(params, ax, num_points=100):
-        angles = np.expand_dims(np.linspace(0, 2*np.pi, num_points), 1)
-        xs, ys = np.cos(angles), np.sin(angles)
-        circle_pts = np.concatenate([xs, ys], axis=1) * 2.0
-        for log_proportion, mean, chol in zip(*unpack_params(params)):
-            cur_pts = mean + np.dot(circle_pts, chol)
-            alpha = np.minimum(1.0, np.exp(log_proportion) * num_guassians)
-            ax.plot(cur_pts[:, 0], cur_pts[:, 1], '-', alpha=alpha)
+    flattened_obj, unflatten, flattened_init_params =\
+        flatten_func(objective, init_params)
 
     fig = plt.figure(figsize=(12,8), facecolor='white')
     ax = fig.add_subplot(111, frameon=False)
     plt.show(block=False)
 
-    def callback(params):
+    def callback(flattened_params):
+        params = unflatten(flattened_params)
         print("Log likelihood {}".format(-objective(params)))
         ax.cla()
         ax.plot(data[:, 0], data[:, 1], 'bx')
@@ -107,7 +80,5 @@ if __name__ == '__main__':
         plt.draw()
         plt.pause(1.0/60.0)
 
-    # Initialize and optimize model.
-    rs = npr.RandomState(0)
-    init_params = rs.randn(num_gmm_params) * 0.1
-    minimize(value_and_grad(objective), init_params, jac=True, method='CG', callback=callback)
+    minimize(value_and_grad(flattened_obj), flattened_init_params,
+             jac=True, method='CG', callback=callback)
