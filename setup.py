c33448315cc53337ddb913bfbe5c5a2269e2d783
David Duvenaud <dduvenaud@seas.harvard.edu>
2016-06-07 21:06:22 -0400

    Changed to new email address

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.1.4',
     description='Efficiently computes derivatives of numpy code.',
     author='Dougal Maclaurin and David Duvenaud and Matthew Johnson',
-    author_email="maclaurin@physics.harvard.edu, dduvenaud@seas.harvard.edu, mattjj@csail.mit.edu",
+    author_email="maclaurin@physics.harvard.edu, duvenaud@cs.toronto.edu, mattjj@csail.mit.edu",
     packages=['autograd', 'autograd.numpy', 'autograd.scipy', 'autograd.scipy.stats'],
     install_requires=['numpy>=1.9', 'future>=0.15.2'],
     setup_requires=['numpy>=1.9'],
