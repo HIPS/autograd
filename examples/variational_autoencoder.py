7b8047b67c41c8046e64c162a5e7b8ec7134ce05
David Duvenaud <dduvenaud@seas.harvard.edu>
2016-09-26 19:09:42 -0400

    Added comments and made variable names better in VAE example

--- examples/variational_autoencoder.py
+++ examples/variational_autoencoder.py
@@ -10,7 +10,6 @@ from autograd import grad
 from optimizers import adam
 from data import load_mnist, save_images
 
-
 def diag_gaussian_log_density(x, mu, log_std):
     return np.sum(norm.logpdf(x, mu, np.exp(log_std)), axis=-1)
 
@@ -23,16 +22,17 @@ def unpack_gaussian_params(params):
 def sample_diag_gaussian(mean, log_std, rs):
     return rs.randn(*mean.shape) * np.exp(log_std) + mean
 
-def bernoulli_log_density(targets, logprobs):
+def bernoulli_log_density(targets, unnormalized_logprobs):
+    # unnormalized_logprobs are in R
     # Targets must be -1 or 1
-    label_probabilities = -np.logaddexp(0, -logprobs*targets)
+    label_probabilities = -np.logaddexp(0, -unnormalized_logprobs*targets)
     return np.sum(label_probabilities, axis=-1)   # Sum across pixels.
 
 def relu(x):    return np.maximum(0, x)
 def sigmoid(x): return 0.5 * (np.tanh(x) + 1)
 
 def init_net_params(scale, layer_sizes, rs=npr.RandomState(0)):
-    """Build a (weights, biases) tuples for each layer."""
+    """Build a (weights, biases) tuples for all layers."""
     return [(scale * rs.randn(m, n),   # weight matrix
              scale * rs.randn(n))      # bias vector
             for m, n in zip(layer_sizes[:-1], layer_sizes[1:])]
@@ -43,20 +43,22 @@ def batch_normalize(activations):
 
 def neural_net_predict(params, inputs):
     """Params is a list of (weights, bias) tuples.
-       inputs is an (N x D) matrix."""
+       inputs is an (N x D) matrix.
+       Applies batch normalization to every layer but the last."""
     for W, b in params[:-1]:
-        outputs = batch_normalize(np.dot(inputs, W) + b)
-        inputs = relu(outputs)
+        outputs = batch_normalize(np.dot(inputs, W) + b)  # linear transformation
+        inputs = relu(outputs)                            # nonlinear transformation
     outW, outb = params[-1]
     outputs = np.dot(inputs, outW) + outb
     return outputs
 
 def nn_predict_gaussian(params, inputs):
+    # Returns means and diagonal variances
     return unpack_gaussian_params(neural_net_predict(params, inputs))
 
-def generate_from_noise(gen_params, num_samples, noise_dim, rs):
-    noise = rs.randn(num_samples, noise_dim)
-    return sigmoid(neural_net_predict(gen_params, noise))
+def generate_from_prior(gen_params, num_samples, noise_dim, rs):
+    latents = rs.randn(num_samples, noise_dim)
+    return sigmoid(neural_net_predict(gen_params, latents))
 
 def p_images_given_latents(gen_params, images, latents):
     preds = neural_net_predict(gen_params, latents)
@@ -76,7 +78,7 @@ def vae_lower_bound(gen_params, rec_params, data, rs):
 if __name__ == '__main__':
     # Model hyper-parameters
     latent_dim = 10
-    data_dim = 784
+    data_dim = 784  # How many pixels in each image (28x28).
     gen_layer_sizes = [latent_dim, 300, 200, data_dim]
     rec_layer_sizes = [data_dim, 200, 300, latent_dim * 2]
 
@@ -104,9 +106,9 @@ if __name__ == '__main__':
     # Define training objective
     seed = npr.RandomState(0)
     def objective(combined_params, iter):
-        idx = batch_indices(iter)
+        data_idx = batch_indices(iter)
         gen_params, rec_params = combined_params
-        return -vae_lower_bound(gen_params, rec_params, train_images[idx], seed) / data_dim
+        return -vae_lower_bound(gen_params, rec_params, train_images[data_idx], seed) / data_dim
 
     # Get gradients of objective using autograd.
     objective_grad = grad(objective)
@@ -118,7 +120,7 @@ if __name__ == '__main__':
             bound = np.mean(objective(combined_params, iter))
             print("{:15}|{:20}".format(iter//num_batches, bound))
 
-            fake_data = generate_from_noise(gen_params, 20, latent_dim, seed)
+            fake_data = generate_from_prior(gen_params, 20, latent_dim, seed)
             save_images(fake_data, 'vae_samples.png', vmin=0, vmax=1)
 
     # The optimizers provided can optimize lists, tuples, or dicts of parameters.
