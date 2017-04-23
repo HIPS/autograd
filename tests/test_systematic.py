cdade4e12ebc3e557f66d07c3dc59ba96600fb6f
Matthew Johnson <mattjj@csail.mit.edu>
2017-04-22 19:28:56 -0700

    make grad_einsum handle more cases by using numpy.core.einsumfunc._parse_einsum_input from numpy 1.12 (at the cost of bumping the numpy version requirement and relying on an internal numpy function, which we could get from the opt_einsum module instead)

--- tests/test_systematic.py
+++ tests/test_systematic.py
@@ -181,6 +181,9 @@ def test_einsum2_matmult_broadcast(): combo_check(np.einsum, [0, 2],
 def test_einsum2_covsum():     combo_check(np.einsum, [0, 2], [R(3, 4, 4)], [(0,1,2)], [R(4, 4, 3)], [(3,1,0)], [(3,2,0)])
 def test_einsum2_three_args(): combo_check(np.einsum, [0, 2],
                                           [R(3, 4, 4)], [(0,1,2)], [R(4, 4, 3)], [(3,1,0)], [R(4, 4, 3)], [(3,3,0)], [(3,2,0)])
+def test_einsum_naked_sum(): combo_check(np.einsum, [1, 2], ['k,nk->'], [R(5)], [R(10, 5)])
+def test_einsum_naked_sum_ellipsis(): combo_check(np.einsum, [1, 2], ['...k,...nk->...'],
+                                                  [R(3, 5)], [R(3, 10, 5)])
 
 def test_trace():    combo_check(np.trace, [0], [R(5, 5), R(4, 5), R(5, 4), R(3, 4, 5)], offset=[-1, 0, 1])
 def test_diag():     combo_check(np.diag, [0], [R(5, 5)], k=[-1, 0, 1])
