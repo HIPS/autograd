f0ede5fd200ad47daf64c95dc5d6162e66a3faf2
Matthew Johnson <mattjj@csail.mit.edu>
2017-05-01 17:05:04 -0700

    import make_tuple, make_list, and make_dict into autograd namespace

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from .core import primitive, make_vjp
 from . import container_types
+from .container_types import make_tuple, make_list, make_dict
 from .convenience_wrappers import (grad, multigrad, multigrad_dict, elementwise_grad,
                                    value_and_grad, grad_and_aux, hessian_vector_product,
                                    hessian, jacobian, vector_jacobian_product, grad_named,
