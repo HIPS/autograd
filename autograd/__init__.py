6b417f884fa9880d2610e644e199e383bb08d051
Hervé BREDIN <bredin@limsi.fr>
2017-05-04 13:43:30 +0200

    feat: add "value_and_multigrad" at root

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -5,4 +5,4 @@ from .container_types import make_tuple, make_list, make_dict
 from .convenience_wrappers import (grad, multigrad, multigrad_dict, elementwise_grad,
                                    value_and_grad, grad_and_aux, hessian_vector_product,
                                    hessian, jacobian, vector_jacobian_product, grad_named,
-                                   checkpoint, make_hvp)
+                                   checkpoint, make_hvp, value_and_multigrad)
