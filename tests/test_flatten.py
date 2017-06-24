2e95805ee2f8ae40b1d9889af898d03f7117afcd
Matthew Johnson <mattjj@csail.mit.edu>
2017-06-24 12:52:24 -0700

    add test that fails on master

--- tests/test_flatten.py
+++ tests/test_flatten.py
@@ -1,7 +1,7 @@
 import autograd.numpy as np
 import autograd.numpy.random as npr
 from autograd.util import flatten
-from autograd import make_vjp
+from autograd import make_vjp, grad
 
 def test_flatten():
     val = (npr.randn(4), [npr.randn(3,4), 2.5], (), (2.0, [1.0, npr.randn(2)]))
@@ -27,3 +27,10 @@ def unflatten_tracing():
     def f(vect): return unflatten(vect)
     flatten2, _ = make_vjp(f)(vect)
     assert np.all(vect == flatten2(val))
+
+def test_flatten_nodes_in_containers():
+    # see issue #232
+    def f(x, y):
+        xy, _ = flatten([x, y])
+        return np.sum(xy)
+    grad(f)(1.0, 2.0)
