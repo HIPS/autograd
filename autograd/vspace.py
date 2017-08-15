b10256d09c1844e9197e6422f595a6dc22628d85
Dougal <d.maclaurin@gmail.com>
2017-08-14 21:57:07 -0400

    Tidied up vspace.py

--- autograd/vspace.py
+++ autograd/vspace.py
@@ -3,69 +3,32 @@ import numpy as np
 import numpy.random as npr
 from .tracer import primitive, isbox
 
-identity_vjp = lambda *args: lambda g: g
-
-@primitive
-def vs_add(vs, x_prev, x_new): return vs._add(x_prev, x_new)
-vs_add.defvjps(identity_vjp, argnums=[1,2])
-
-@primitive
-def vs_mut_add(vs, x_prev, x_new): return vs._mut_add(x_prev, x_new)
-vs_mut_add.defvjps(identity_vjp, argnums=[1,2])
-
-@primitive
-def vs_covector(vs, x): return vs._covector(x)
-vs_covector.defvjp(lambda ans, vs, gvs, vs_, x: lambda g:
-                   gvs.covector(g), argnum=1)
-
-@primitive
-def vs_scalar_mul(vs, x, a):
-    return vs._scalar_mul(x, a)
-vs_scalar_mul.defvjp(lambda ans, vs, gvs, vs_, x, a: lambda g:
-                     vs.covector(gvs.scalar_mul(gvs.covector(g), a)), argnum=1)
-vs_scalar_mul.defvjp(lambda ans, vs, gvs, vs_, x, a: lambda g:
-                     gvs.inner_prod(g, gvs.covector(x)), argnum=2)
-
-@primitive
-def vs_inner_prod(vs, x, y):
-    return vs._inner_prod(x, y)
-vs_inner_prod.defvjp(lambda ans, vs, gvs, vs_, x, y: lambda g:
-                     vs.covector(vs.scalar_mul(y, gvs.covector(g))), argnum=1)
-vs_inner_prod.defvjp(lambda ans, vs, gvs, vs_, x, y: lambda g:
-                     vs.covector(vs.scalar_mul(x, gvs.covector(g))), argnum=2)
-
 class VSpace(object):
     __slots__ = []
     iscomplex = False
-    def __init__(self, value):
-        pass
+    def __init__(self, value): pass
 
     def zeros(self):          assert False
     def ones(self):           assert False
     def standard_basis(self): assert False
     def randn(self):          assert False
 
-    add        = vs_add
-    mut_add    = vs_mut_add
-    scalar_mul = vs_scalar_mul
-    inner_prod = vs_inner_prod
-    covector   = vs_covector
+    @primitive
+    def add(vs, x_prev, x_new):     return vs._add(x_prev, x_new)
+    @primitive
+    def mut_add(vs, x_prev, x_new): return vs._mut_add(x_prev, x_new)
+    @primitive
+    def scalar_mul(vs, x, a):       return vs._scalar_mul(x, a)
+    @primitive
+    def inner_prod(vs, x, y):       return vs._inner_prod(x, y)
+    @primitive
+    def covector(vs, x):            return vs._covector(x)
 
-    def _add(self, x, y):
-        return x + y
-
-    def _mut_add(self, x, y):
-        x += y
-        return x
-
-    def _covector(self, x):
-        return x
-
-    def _scalar_mul(self, x, a):
-        return x * a
-
-    def _inner_prod(self, x, y):
-        assert False
+    def _add(self, x, y):        return x + y
+    def _mut_add(self, x, y):    x += y; return x
+    def _scalar_mul(self, x, a): return x * a
+    def _inner_prod(self, x, y): assert False
+    def _covector(self, x):      return x
 
     def __eq__(self, other):
         return type(self) == type(other) and self.__dict__ == other.__dict__
@@ -81,6 +44,20 @@ class VSpace(object):
         unit_vect = self.unflatten(unit_vect)
         rand_vect = npr.randn(N)
         return [self.zeros(), self.unflatten(npr.randn(N))]
+
+identity_vjp = lambda *args: lambda g: g
+VSpace.mut_add.defvjps(identity_vjp, argnums=[1,2])
+VSpace.inner_prod.defvjp(lambda ans, vs, gvs, vs_, x, y: lambda g:
+                         vs.covector(vs.scalar_mul(y, gvs.covector(g))), argnum=1)
+VSpace.inner_prod.defvjp(lambda ans, vs, gvs, vs_, x, y: lambda g:
+                         vs.covector(vs.scalar_mul(x, gvs.covector(g))), argnum=2)
+VSpace.add.defvjps(identity_vjp, argnums=[1,2])
+VSpace.covector.defvjp(lambda ans, vs, gvs, vs_, x: lambda g:
+                       gvs.covector(g), argnum=1)
+VSpace.scalar_mul.defvjp(lambda ans, vs, gvs, vs_, x, a: lambda g:
+                         vs.covector(gvs.scalar_mul(gvs.covector(g), a)), argnum=1)
+VSpace.scalar_mul.defvjp(lambda ans, vs, gvs, vs_, x, a: lambda g:
+                         gvs.inner_prod(g, gvs.covector(x)), argnum=2)
 
 def vspace_flatten(value, covector=False):
     return vspace(value).flatten(value, covector)
