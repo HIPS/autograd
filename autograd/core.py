43761c1854f49357570aa1c2ec48a873c7a1a3ec
Dougal <d.maclaurin@gmail.com>
2017-09-08 20:42:59 -0400

    Experimenting with getting rid of vspace computation after every primitive evaluation. Add_outgrads computes its own vspaces for nodes of outdegree >= 2.

--- autograd/core.py
+++ autograd/core.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from functools import partial
 from .tracer import (trace, primitive, notrace_primitive, Node, Box,
                      register_box, toposort)
-from .vspace import vspace, assert_vspace_match, register_vspace, VSpace
+from .vspace import vspace, register_vspace, VSpace
 from .util import func, subval
 
 def make_vjp(fun, x):
@@ -15,14 +15,12 @@ def make_vjp(fun, x):
     return vjp, end_value
 
 def backward_pass(g, end_node):
-    assert_vspace_match(g, end_node.vspace)
     outgrads = {end_node : (g, False)}
     for node in toposort(end_node):
         cur_outgrad = outgrads.pop(node)
         for parent, vjp in node.parents_and_vjps:
             outgrad = vjp(cur_outgrad[0])
-            assert_vspace_match(outgrad, parent.vspace)
-            outgrads[parent] = add_outgrads(parent.vspace, outgrads.get(parent), outgrad)
+            outgrads[parent] = add_outgrads(outgrads.get(parent), outgrad)
     return cur_outgrad[0]
 
 def make_jvp(fun, x):
@@ -38,39 +36,32 @@ def make_jvp(fun, x):
 class VJPNode(Node):
     __slots__ = ['vspace', 'parents', 'parents_and_vjps']
     def __init__(self, value, fun, args, kwargs, parent_argnums, parents):
-        self.vspace = vspace(value)
         self.parents = parents
         self.parents_and_vjps = [
-            (parent, primitive_vjp(fun, argnum, value, parent.vspace,
-                                   self.vspace, args, kwargs))
+            (parent, primitive_vjp(fun, argnum, value, args, kwargs))
             for argnum, parent in zip(parent_argnums, parents)]
 
     def initialize_root(self, value):
-        self.vspace = vspace(value)
         self.parents = []
         self.parents_and_vjps = []
 
 class JVPNode(Node):
     __slots__ = ['vspace', 'g']
     def __init__(self, value, fun, args, kwargs, parent_argnums, parents):
-        self.vspace = vspace(value)
         cur_g = None
         for argnum, parent in zip(parent_argnums, parents):
-            new_g = primitive_jvp(fun, argnum, parent.g, value, parent.vspace,
-                                  self.vspace, args, kwargs)
-            assert_vspace_match(new_g, self.vspace)
-            cur_g = add_outgrads(self.vspace, cur_g, new_g)
+            new_g = primitive_jvp(fun, argnum, parent.g, value, args, kwargs)
+            cur_g = add_outgrads(cur_g, new_g)
 
         self.g = cur_g[0]
 
     def initialize_root(self, x, g):
-        self.vspace = vspace(x)
-        assert_vspace_match(g, self.vspace)
         self.g = g
 
-def add_outgrads(vs, prev_g_flagged, g):
+def add_outgrads(prev_g_flagged, g):
     sparse = type(g) in sparse_object_types
     if prev_g_flagged:
+        vs = vspace(g)
         prev_g, mutable = prev_g_flagged
         if mutable:
             if sparse:
@@ -85,7 +76,7 @@ def add_outgrads(vs, prev_g_flagged, g):
                 return vs.add(prev_g, g), True
     else:
         if sparse:
-            return sparse_add(vs.zeros(), g), True
+            return sparse_add(vspace(g).zeros(), g), True
         else:
             return g, False
 
@@ -105,7 +96,7 @@ def zero_vjp(ans, vs, gvs, *args, **kwargs):
 
 primitive_vjps = defaultdict(dict)
 
-def primitive_vjp(fun, argnum, ans, vs, gvs, args, kwargs):
+def primitive_vjp(fun, argnum, ans, args, kwargs):
     try:
         vjp = primitive_vjps[fun][argnum]
     except KeyError:
@@ -114,7 +105,7 @@ def primitive_vjp(fun, argnum, ans, vs, gvs, args, kwargs):
         else:
             errstr = "Gradient of {0} not yet implemented."
         raise NotImplementedError(errstr.format(repr(fun), argnum))
-    return vjp(ans, vs, gvs, args, kwargs)
+    return vjp(ans, vspace(args[argnum]), vspace(ans), args, kwargs)
 
 def defvjp(fun, vjpmaker, argnum=0):
     def vjp_fixed_args(ans, vs, gvs, args, kwargs):
@@ -159,9 +150,9 @@ defvjp(func(VSpace.scalar_mul), lambda ans, vs, gvs, vs_, x, a: lambda g:
 defvjp(func(VSpace.scalar_mul), lambda ans, vs, gvs, vs_, x, a: lambda g:
        gvs.inner_prod(g, gvs.covector(x)), argnum=2)
 
-def primitive_jvp(fun, argnum, g, ans, gvs, vs, args, kwargs):
+def primitive_jvp(fun, argnum, g, ans, args, kwargs):
     try:
-        return primitive_jvps[fun][argnum](g, ans, gvs, vs, args, kwargs)
+        return primitive_jvps[fun][argnum](g, ans, vspace(args[argnum]), vspace(ans), args, kwargs)
     except KeyError:
         raise NotImplementedError("JVP of {} wrt arg number {} not yet implemented"
                                   .format(fun.__name__, argnum))
--- autograd/test_util.py
+++ autograd/test_util.py
@@ -29,6 +29,7 @@ def check_vjp(f, x):
     x_v, y_v = x_vs.randn(), y_vs.randn()
 
     vjp_y = x_vs.covector(vjp(y_vs.covector(y_v)))
+    assert vspace(vjp_y) == x_vs
     vjv_numeric = x_vs.inner_prod(x_v, vjp_y)
     vjv_exact   = y_vs.inner_prod(y_v, jvp(x_v))
     assert scalar_close(vjv_numeric, vjv_exact), \
