symmetrize scipy.multivariate_normal.logpdf covariance matrix vjp, fixes #296

autograd/scipy/stats/multivariate_normal.py
@@ -4,6 +4,7 @@ import scipy.stats
 import autograd.numpy as np
 from autograd.core import primitive
 from autograd.numpy.numpy_grads import unbroadcast
+from autograd.numpy.linalg import grad_cholesky
 
 pdf    =  primitive(scipy.stats.multivariate_normal.pdf)
 logpdf =  primitive(scipy.stats.multivariate_normal.logpdf)
@@ -16,31 +17,18 @@ entropy = primitive(scipy.stats.multivariate_normal.entropy)
 # by Mike Giles
 # https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf
 
-def lower_half(mat):
-    # Takes the lower half of the matrix, and half the diagonal.
-    # Necessary since numpy only uses lower half of covariance matrix.
-    if len(mat.shape) == 2:
-        return 0.5 * (np.tril(mat) + np.triu(mat, 1).T)
-    elif len(mat.shape) == 3:
-        return 0.5 * (np.tril(mat) + np.swapaxes(np.triu(mat, 1), 1,2))
-    else:
-        raise ArithmeticError
-
-def generalized_outer_product(mat):
-    if len(mat.shape) == 1:
-        return np.outer(mat, mat)
-    elif len(mat.shape) == 2:
-        return np.einsum('ij,ik->ijk', mat, mat)
-    else:
-        raise ArithmeticError
+def generalized_outer_product(x):
+    if np.ndim(x) == 1:
+        return np.outer(x, x)
+    return np.matmul(x, np.swapaxes(x, -1, -2))
 
 def covgrad(x, mean, cov, allow_singular=False):
     if allow_singular:
         raise NotImplementedError("The multivariate normal pdf is not "
                 "differentiable w.r.t. a singular covariance matix")
-    # I think once we have Cholesky we can make this nicer.
-    solved = np.linalg.solve(cov, (x - mean).T).T
-    return lower_half(np.linalg.inv(cov) - generalized_outer_product(solved))
+    J = np.linalg.inv(cov)
+    solved = np.matmul(J, np.expand_dims(x - mean, -1))
+    return 1./2 * (generalized_outer_product(solved) - J)
 
 def solve(allow_singular):
     if allow_singular:
@@ -50,12 +38,12 @@ def solve(allow_singular):
 
 logpdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs, -np.expand_dims(g, 1) * solve(allow_singular)(cov, (x - mean).T).T), argnum=0)
 logpdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs,  np.expand_dims(g, 1) * solve(allow_singular)(cov, (x - mean).T).T), argnum=1)
-logpdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs, -np.reshape(g, np.shape(g) + (1, 1)) * covgrad(x, mean, cov, allow_singular)), argnum=2)
+logpdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs,  np.reshape(g, np.shape(g) + (1, 1)) * covgrad(x, mean, cov, allow_singular)), argnum=2)
 
 # Same as log pdf, but multiplied by the pdf (ans).
 pdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs, -np.expand_dims(ans * g, 1) * solve(allow_singular)(cov, (x - mean).T).T), argnum=0)
 pdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs,  np.expand_dims(ans * g, 1) * solve(allow_singular)(cov, (x - mean).T).T), argnum=1)
-pdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs, -np.reshape(ans * g, np.shape(g) + (1, 1)) * covgrad(x, mean, cov, allow_singular)), argnum=2)
+pdf.defvjp(lambda g, ans, vs, gvs, x, mean, cov, allow_singular=False: unbroadcast(vs, gvs,  np.reshape(ans * g, np.shape(g) + (1, 1)) * covgrad(x, mean, cov, allow_singular)), argnum=2)
 
 entropy.defvjp_is_zero(argnums=(0,))
 entropy.defvjp(lambda g, ans, vs, gvs, mean, cov: unbroadcast(vs, gvs, 0.5 * g * np.linalg.inv(cov).T), argnum=1)

tests/test_scipy.py
@@ -1,7 +1,18 @@
 from __future__ import absolute_import
 import numpy as npo
+import autograd.numpy as anp
 from builtins import range
 import itertools
+from functools import wraps
+
+def symmetrize_matrix_arg(fun, argnum):
+    def T(X): return anp.swapaxes(X, -1, -2) if anp.ndim(X) > 1 else X
+    def symmetrize(X): return 0.5 * (X + T(X))
+    def symmetrized_fun(*args, **kwargs):
+        args = list(args)
+        args[argnum] = symmetrize(args[argnum])
+        return fun(*args, **kwargs)
+    return symmetrized_fun
 
 try:
     import scipy
@@ -48,8 +59,8 @@ else:
     def test_t_logcdf_broadcast(): combo_check(stats.t.logcdf, [0,2],     [R(4,3)], [R(1,3)**2 + 2.1], [R(4,3)], [R(4,1)**2 + 2.1])
 
     def make_psd(mat): return np.dot(mat.T, mat) + np.eye(mat.shape[0])
-    def test_mvn_pdf():    combo_check(mvn.pdf, [0, 1, 2], [R(4)], [R(4)], [make_psd(R(4, 4))], allow_singular=[False])
-    def test_mvn_logpdf(): combo_check(mvn.logpdf, [0, 1, 2], [R(4)], [R(4)], [make_psd(R(4, 4))], allow_singular=[False])
+    def test_mvn_pdf():    combo_check(symmetrize_matrix_arg(mvn.pdf, 2), [0, 1, 2], [R(4)], [R(4)], [make_psd(R(4, 4))], allow_singular=[False])
+    def test_mvn_logpdf(): combo_check(symmetrize_matrix_arg(mvn.logpdf, 2), [0, 1, 2], [R(4)], [R(4)], [make_psd(R(4, 4))], allow_singular=[False])
     def test_mvn_entropy():combo_check(mvn.entropy,[0, 1],            [R(4)], [make_psd(R(4, 4))])
 
     C = np.zeros((4, 4))
@@ -58,8 +69,8 @@ else:
     def test_mvn_pdf_sing_cov(): combo_check(mvn.pdf, [0, 1], [np.concatenate((R(2), np.zeros(2)))], [np.concatenate((R(2), np.zeros(2)))], [C], [True])
     def test_mvn_logpdf_sing_cov(): combo_check(mvn.logpdf, [0, 1], [np.concatenate((R(2), np.zeros(2)))], [np.concatenate((R(2), np.zeros(2)))], [C], [True])
 
-    def test_mvn_pdf_broadcast():    combo_check(mvn.pdf, [0, 1, 2], [R(5, 4)], [R(4)], [make_psd(R(4, 4))])
-    def test_mvn_logpdf_broadcast(): combo_check(mvn.logpdf, [0, 1, 2], [R(5, 4)], [R(4)], [make_psd(R(4, 4))])
+    def test_mvn_pdf_broadcast():    combo_check(symmetrize_matrix_arg(mvn.pdf, 2), [0, 1, 2], [R(5, 4)], [R(4)], [make_psd(R(4, 4))])
+    def test_mvn_logpdf_broadcast(): combo_check(symmetrize_matrix_arg(mvn.logpdf, 2), [0, 1, 2], [R(5, 4)], [R(4)], [make_psd(R(4, 4))])
 
     alpha = npr.random(4)**2 + 1.2
     x = stats.dirichlet.rvs(alpha, size=1)[0,:]