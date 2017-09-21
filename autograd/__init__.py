c36319aaf8d0b38f5c9dd2c0189561c94d03043e
Dougal <d.maclaurin@gmail.com>
2017-09-21 00:47:45 -0400

    Renamed convenience_wrappers.py -> differential_operators.py

--- autograd/__init__.py
+++ autograd/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from .convenience_wrappers import (
+from .differential_operators import (
     make_vjp, grad, multigrad_dict, elementwise_grad, value_and_grad,
     grad_and_aux, hessian_tensor_product, hessian_vector_product, hessian,
     jacobian, tensor_jacobian_product, vector_jacobian_product, grad_named,
