aee0c817ed699448525bcd76ceaa6f0f8f4965ec
Gianluca Campanella <gianluca@campanella.org>
2017-10-17 17:22:33 +0100

    Added unbroadcast() to Poisson VJPs

--- autograd/scipy/stats/poisson.py
+++ autograd/scipy/stats/poisson.py
@@ -4,6 +4,7 @@ import autograd.numpy as np
 import scipy.stats
 
 from autograd.core import primitive
+from autograd.numpy.numpy_grads import unbroadcast
 
 cdf = primitive(scipy.stats.poisson.cdf)
 logpmf = primitive(scipy.stats.poisson.logpmf)
@@ -12,6 +13,6 @@ pmf = primitive(scipy.stats.poisson.pmf)
 def grad_poisson_logpdf(k, mu):
     return np.where(k % 1 == 0, k / mu - 1, 0)
 
-cdf.defvjp(lambda g, ans, vs, gvs, k, mu: g * -pmf(np.floor(k), mu), argnum=1)
-logpmf.defvjp(lambda g, ans, vs, gvs, k, mu: g * grad_poisson_logpdf(k, mu), argnum=1)
-pmf.defvjp(lambda g, ans, vs, gvs, k, mu: g * ans * grad_poisson_logpdf(k, mu), argnum=1)
+cdf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * -pmf(np.floor(k), mu)), argnum=1)
+logpmf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * grad_poisson_logpdf(k, mu)), argnum=1)
+pmf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * ans * grad_poisson_logpdf(k, mu)), argnum=1)
