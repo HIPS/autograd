2cc1f8a830de72b5fd4e1df7384f0fdb90ac12b5
Gianluca Campanella <gianluca@campanella.org>
2017-10-20 11:07:37 +0100

    Fixed PDF -> PMF

--- autograd/scipy/stats/poisson.py
+++ autograd/scipy/stats/poisson.py
@@ -10,9 +10,9 @@ cdf = primitive(scipy.stats.poisson.cdf)
 logpmf = primitive(scipy.stats.poisson.logpmf)
 pmf = primitive(scipy.stats.poisson.pmf)
 
-def grad_poisson_logpdf(k, mu):
+def grad_poisson_logpmf(k, mu):
     return np.where(k % 1 == 0, k / mu - 1, 0)
 
 cdf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * -pmf(np.floor(k), mu)), argnum=1)
-logpmf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * grad_poisson_logpdf(k, mu)), argnum=1)
-pmf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * ans * grad_poisson_logpdf(k, mu)), argnum=1)
+logpmf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * grad_poisson_logpmf(k, mu)), argnum=1)
+pmf.defvjp(lambda g, ans, vs, gvs, k, mu: unbroadcast(vs, gvs, g * ans * grad_poisson_logpmf(k, mu)), argnum=1)
