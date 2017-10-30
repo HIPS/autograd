1a03ace4e1817e01bca238f8892c7cb1c8445ffe
Matthew Johnson <mattjj@csail.mit.edu>
2017-10-29 17:57:38 -0700

    improve example-testing script

--- tests/check_examples_run.sh
+++ tests/check_examples_run.sh
@@ -2,14 +2,38 @@
 
 PYTHONPATH=".:$PYTHONPATH"
 
-for f in examples/*.py; do
-    timeout 15s python $f > /dev/null
+working=()
+failing=()
+
+examples=$(find examples -name '*.py' -not -name '__init__.py')
+
+echo 'Running all the examples...'
+for f in $examples; do
+    timeout 15s python $f > /dev/null 2>&1
     status=$?
     if [ $status -eq 0 -o $status -eq 124 ]; then
         echo $f "seems to work"
+        working+=($f)
     elif [ $status -eq 137 ]; then
         echo $f "might be working, but had to be killed"
+        working+=($f)
     else
         echo $f "seems broken, try running manually"
+        failing+=($f)
     fi
 done
+
+if [ ! ${#working[@]} -eq 0 ]; then
+    echo -e '\033[01;36m'
+    echo "These seemed to WORK:"
+    echo -en '\033[00m'
+    printf '%s\n' "${working[@]}"
+    echo
+fi
+if [ ! ${#failing[@]} -eq 0 ]; then
+    echo -e '\033[01;31m'
+    echo "These seemed to FAIL:"
+    echo -en '\033[00m'
+    printf '%s\n' "${failing[@]}"
+    echo
+fi
