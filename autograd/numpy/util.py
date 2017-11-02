1ba7015b3692fb890fc3299f18ae0cd082bf88c0
Jamie Townsend <jamiehntownsend@gmail.com>
2017-11-02 16:28:33 +0000

    Simplify def_ufunc_jps api

--- autograd/numpy/util.py
+++ autograd/numpy/util.py
@@ -27,37 +27,47 @@ def def_ufunc_jps(ufunc, *derivs_ops):
     derivs_ops = list(derivs_ops)
 
     unary_ufunc_jps = {
-        'same': (lambda deriv: lambda g, ans, x:        ufunc(g),
-                 lambda deriv: lambda ans, x: ufunc),
+        'same': (lambda g, ans, x:        ufunc(g),
+                 lambda ans, x: ufunc),
+        'cid':  (lambda g, ans, x:        match_complex(ans, g),
+                 lambda ans, x: lambda g: match_complex(x  , g))
+        }
+
+    unary_ufunc_linops = {
         'mul' : (lambda deriv: lambda g, ans, x:        g * deriv(ans, x),
                  lambda deriv: lambda ans, x: lambda g, d=deriv(ans, x): g * d),
         'div' : (lambda deriv: lambda g, ans, x:        g / deriv(ans, x),
                  lambda deriv: lambda ans, x: lambda g, d=deriv(ans, x): g / d),
         'cmul': (lambda deriv: lambda g, ans, x:        match_complex(ans, g * deriv(ans, x)),
                  lambda deriv: lambda ans, x: lambda g, d=deriv(ans, x): match_complex(x, g * d)),
-        'cid':  (lambda deriv: lambda g, ans, x:        match_complex(ans, g),
-                 lambda deriv: lambda ans, x: lambda g: match_complex(x  , g))
         }
 
     if len(derivs_ops) == 1:
-        if derivs_ops[0] is None:
+        deriv_op = derivs_ops[0]
+        if type(deriv_op) is tuple:
+            deriv, op = deriv_op
+            defjvp(ufunc, unary_ufunc_linops[op][0](deriv))
+            defvjp(ufunc, unary_ufunc_linops[op][1](deriv))
+        elif deriv_op is None:
             defjvp(ufunc, None)
             defvjp(ufunc, None)
         else:
-            deriv, op = derivs_ops[0]
-            defjvp(ufunc, unary_ufunc_jps[op][0](deriv))
-            defvjp(ufunc, unary_ufunc_jps[op][1](deriv))
+            defjvp(ufunc, unary_ufunc_jps[deriv_op][0])
+            defvjp(ufunc, unary_ufunc_jps[deriv_op][1])
 
     nary_ufunc_jps = {
-        'same': (lambda argnum, deriv: lambda g, ans, *args: ufunc(*subval(args, argnum, g)),
-                 lambda argnum, deriv: lambda ans, *args:
+        'same': (lambda argnum: lambda g, ans, *args: ufunc(*subval(args, argnum, g)),
+                 lambda argnum: lambda ans, *args:
                      unbroadcast_f(args[argnum], lambda g: ufunc(*subval(args, argnum, g)))),
-        'id':   (lambda argnum, deriv: lambda g, ans, *args: match_complex(ans, anp.broadcast_to(g, ans.shape)),
-                 lambda argnum, deriv: lambda ans, *args:
+        'id':   (lambda argnum: lambda g, ans, *args: match_complex(ans, anp.broadcast_to(g, ans.shape)),
+                 lambda argnum: lambda ans, *args:
                      unbroadcast_f(args[argnum], lambda g: match_complex(args[argnum], g))),
-        'neg':  (lambda argnum, deriv: lambda g, ans, *args: match_complex(ans, anp.broadcast_to(-g, ans.shape)),
-                 lambda argnum, deriv: lambda ans, *args:
-                     unbroadcast_f(args[argnum], lambda g: match_complex(args[argnum], -g))),
+        'neg':  (lambda argnum: lambda g, ans, *args: match_complex(ans, anp.broadcast_to(-g, ans.shape)),
+                 lambda argnum: lambda ans, *args:
+                     unbroadcast_f(args[argnum], lambda g: match_complex(args[argnum], -g)))
+        }
+
+    nary_ufunc_linops = {
         'mul':  (lambda argnum, deriv: lambda g, ans, *args: g * deriv(ans, *args),
                  lambda argnum, deriv: lambda ans, *args:
                      unbroadcast_f(args[argnum], lambda g, d=deriv(ans, *args): g * d)),
@@ -65,9 +75,16 @@ def def_ufunc_jps(ufunc, *derivs_ops):
                  lambda argnum, deriv: lambda ans, *args:
                      unbroadcast_f(args[argnum], lambda g, d=deriv(ans, *args): g / d))
         }
-    if len(derivs_ops) >= 2:
-        defjvp(ufunc, *[nary_ufunc_jps[deriv_op[1]][0](argnum, deriv_op[0])
-                        if deriv_op is not None else None for argnum, deriv_op in enumerate(derivs_ops)])
-        defvjp(ufunc, *[nary_ufunc_jps[deriv_op[1]][1](argnum, deriv_op[0])
-                        if deriv_op is not None else None for argnum, deriv_op in enumerate(derivs_ops)])
 
+    def deriv_op_to_ufunc_jp(idx, argnum, deriv_op):
+        if type(deriv_op) is tuple:
+            deriv, op = deriv_op
+            return nary_ufunc_linops[op][idx](argnum, deriv)
+        elif deriv_op is None:
+            return None
+        else:
+            return nary_ufunc_jps[deriv_op][idx](argnum)
+
+    if len(derivs_ops) >= 2:
+        defjvp(ufunc, *[deriv_op_to_ufunc_jp(0, argnum, deriv_op) for argnum, deriv_op in enumerate(derivs_ops)])
+        defvjp(ufunc, *[deriv_op_to_ufunc_jp(1, argnum, deriv_op) for argnum, deriv_op in enumerate(derivs_ops)])
