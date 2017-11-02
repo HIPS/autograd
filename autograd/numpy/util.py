486eceab319b96808642f432cc6e39cc745e0a14
Jamie Townsend <jamiehntownsend@gmail.com>
2017-11-02 22:49:43 +0000

    Add docstring to def_ufunc_jps

--- autograd/numpy/util.py
+++ autograd/numpy/util.py
@@ -88,6 +88,78 @@ def def_nary_ufunc_jps(ufunc, derivs_ops):
                     for argnum, deriv_op in enumerate(derivs_ops)])
 
 def def_ufunc_jps(ufunc, *derivs_ops):
+    """
+    Specify the derivatives of ufunc. Once this has been done the ufunc will
+    support both reverse and forward mode differentiation.
+
+    The derivatives can be specified as follows.
+
+    Unary ufuncs
+    ------------
+    If the ufunc is unary (that is, if it takes one array valued argument),
+    then a single optional argument is required to specify the ufunc's
+    derivative.
+    
+    In the general case, this is done via a pair (deriv, op), where deriv is a
+    function taking in the output of the ufunc (ans), and its array argument
+    (x), and returning the derivative of the ufunc.
+    
+    Here 'derivative' means the elementwise derivative of the ufunc w.r.t. it's
+    input.
+
+    For example, for the ufunc np.sin, this is as simple as
+    >>> def deriv(ans, x):
+    ...     return np.cos(x)
+    ...
+
+    Sometimes the output of the ufunc is useful, for example the derivative of
+    np.exp is np.exp, which is identical to ans, so the derivative of np.exp
+    can be efficiently implemented as
+    >>> def deriv(ans, x):
+    ...     return ans
+    ...
+
+    The other element of the pair is `op`, which should usually be set to
+    'mul'. However, if the derivative of the ufunc is of the form
+    1 / f(ans, x), then you can save some computation by using the pair
+    (f, 'div') to specify the derivative. The 'div' flags that the gradients
+    being propagated through this primitive should be divided by the result of
+    f, not multiplied.
+
+    Some full examples:
+    >>> def_ufunc_jps(np.sin, (lambda ans, x: np.cos(x), 'mul'))
+    >>> def_ufunc_jps(np.exp, (lambda ans, x: ans,       'mul'))
+    >>> def_ufunc_jps(np.log, (lambda ans, x: x,         'div'))
+
+    Special cases
+    -------------
+    If the derivative of the ufunc is a constant, then you don't need to
+    specify its derivative and you can use just the string 'same' in place of
+    the pair (deriv, op). This says that its ok to propagate the gradient
+    through this primitive by applying the ufunc itself to the gradient, and
+    neither x nor ans are relevant to this computation. 
+
+    For example, the derivative of np.negative (which simply negates its
+    inputs), is -1, so
+    >>> def_ufunc_jps(np.negative, 'same')
+
+    will correctly set its derivative.
+
+    N-ary ufuncs
+    ------------
+    For ufuncs which take more than one array argument, the derivatives can be
+    specified by passing one (deriv, op) pair for each argument (you can use
+    None as a placeholder for args whose derivative you don't wish to define).
+
+    You can use 'same' in exactly the same way as for unary ufuncs, and
+    additionally you can use 'id' when the derivative w.r.t. an arg is always
+    equal to 1, and 'neg' when it's always equal to -1.
+
+    Some examples:
+    >>> def_ufunc_jps(anp.divide,   'same', (lambda ans, x, y: -ans/y, 'mul'))
+    >>> def_ufunc_jps(anp.add,      'id', 'id')
+    >>> def_ufunc_jps(anp.subtract, 'id', 'neg')
+    """
     derivs_ops = list(derivs_ops)
     if len(derivs_ops) == 1:
         def_unary_ufunc_jps(ufunc, derivs_ops[0])
