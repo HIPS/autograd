12561ddf4fb1b27deb41177e8a37f2460a2f996c
Eric Ma <ericmajinglong@gmail.com>
2018-05-29 01:10:00 -0400

    added integration test on gradient descent

--- tests/test_cupy_hack.py
+++ tests/test_cupy_hack.py
@@ -1,7 +1,8 @@
 import autograd.cupy as cp
 from autograd import elementwise_grad as egrad
 import pytest
-
+from autograd import grad
+import pdb
 
 @pytest.mark.integration
 def test_sin():
@@ -31,7 +32,6 @@ def test_higher_order_derivatives():
     dddf(a)
 
 
-
 @pytest.mark.integration
 def test_linalg_norm():
     """All that I'm asking of this test is that it doesn't error out."""
@@ -45,3 +45,24 @@ def test_argmin():
     x = cp.arange(10)
     cp.argmin(x)
 
+
+@pytest.mark.integration
+@pytest.mark.gradient
+def test_gradient_descent():
+    x = cp.array([[1,2,3,4,5,6,7,8]])
+    y = cp.array([[-1,-1,-1,-1,1,1,1,1]])
+
+    def model(w, x):
+        a = w[0] + cp.dot(x.T,w[1:])
+        return a.T
+
+    def softmax(w):
+        cost = cp.sum(cp.log(1 + cp.exp(-y*model(w, x))))
+        return cost/float(cp.size(y))
+
+    w = cp.random.rand(2,1)
+
+    gradient = grad(softmax)
+
+    a = gradient(w)
+    print(a)
