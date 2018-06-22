fd7df486baeb4d8bbed5e0f1599cb7738d9501b9
Eric Ma <ericmajinglong@gmail.com>
2018-06-22 16:57:25 -0400

    added first test, still failing

--- tests/test_cupy_sparse.py
+++ tests/test_cupy_sparse.py
@@ -4,34 +4,40 @@ import pytest
 from autograd import grad
 import pdb
 import numpy as np
+from autograd.test_util import check_grads
 
 
 # ----- pytest fixture for sparse arrays ----- #
 @pytest.fixture
-def sparse():
-    data = cp.array([1, 2, 3]).astype('float32')
-    rows = cp.array([1, 2, 3]).astype('float32')
-    cols = cp.array([1, 3, 4]).astype('float32')
-
-    return cp.sparse.coo_matrix((data, (rows, cols)))
-    
+def eye():
+    return cp.sparse.eye(5).tocsr()
 
 
 # ----- tests for array creation ----- #
 @pytest.mark.works
 @pytest.mark.cupy_sparse
-def test_sparse_coo_matrix():
+def test_sparse_coo_matrix(sparse):
     """This just has to not error out."""
     data = cp.array([1, 2, 3]).astype('float32')
     rows = cp.array([1, 2, 3]).astype('float32')
     cols = cp.array([1, 3, 4]).astype('float32')
-    a = cp.sparse.coo_matrix((data, (rows, cols)))
+    print(sparse.shape)
 
 
 # ----- tests for array multiplication ----- #
-@pytest.mark.test
 @pytest.mark.cupy_sparse
 def test_sparse_dense_multiplication(sparse):
     """This just has to not error out."""
-    dense = cp.random.random(size=(3, 4))
-    cp.dot(sparse, dense)
+    dense = cp.random.random(size=(5, 4))
+    sparse.dot(dense)
+
+
+@pytest.mark.test
+@pytest.mark.cupy_sparse
+def test_sparse_dot(eye):
+    dense = cp.random.random(size=(5, 5))
+
+    def fun(x):
+        return cp.sparse.dot(x, dense)
+
+    check_grads(fun)(eye)
