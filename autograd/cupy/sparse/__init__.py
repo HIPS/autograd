79fa2f75a9447921d482648e18b2d323d8358e2c
Eric Ma <ericmajinglong@gmail.com>
2018-06-22 16:56:19 -0400

    fixed sparse wrapper import

--- autograd/cupy/sparse/__init__.py
+++ autograd/cupy/sparse/__init__.py
@@ -1,1 +1,6 @@
-from .sparse_wrapper import csc_matrix, csr_matrix, coo_matrix, dia_matrix
+from .sparse_wrapper import *
+from . import sparse_vjps
+from . import sparse_jvps
+from . import sparse_vspaces
+from . import sparse_boxes
+
