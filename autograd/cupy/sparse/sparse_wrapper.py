e16140d5a8c2339c44a963735248405c9c00405c
Eric Ma <ericmajinglong@gmail.com>
2018-06-22 16:57:08 -0400

    implemented some sparse methods

--- autograd/cupy/sparse/sparse_wrapper.py
+++ autograd/cupy/sparse/sparse_wrapper.py
@@ -1,6 +1,6 @@
 import cupy.sparse as _sp
 from autograd.extend import primitive
-
+import cupy as _cp
 
 # ----- definition for coo_matrix ----- #
 def coo_matrix(arg1, *args, **kwargs):
@@ -31,3 +31,14 @@ def sparse_matrix_from_args(type, arg1, *args, **kwargs):
         return _sp.csc_matrix(arg1, *args, **kwargs)
     elif type == 'dia':
         return _sp.dia_matrix(arg1, *args, **kwargs)
+
+
+@primitive
+def dot(sparse, dense):
+    assert not isinstance(sparse, _cp.ndarray), 'the sparse array must be the first argument'
+    return sparse.dot(dense)
+
+
+@primitive
+def eye(N):
+    return _cp.sparse.eye(N)
