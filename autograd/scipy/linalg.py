ee3562d50a6fb4a65be379a5697fd0c2fb1eb130
Jamie Townsend <jamiehntownsend@gmail.com>
2018-11-27 18:02:39 +0000

    Add solve_sylvester vjp, jvp

--- autograd/scipy/linalg.py
+++ autograd/scipy/linalg.py
@@ -3,7 +3,7 @@ import scipy.linalg
 
 import autograd.numpy as anp
 from autograd.numpy.numpy_wrapper import wrap_namespace
-from autograd.extend import defvjp, defjvp
+from autograd.extend import defvjp, defvjp_argnums, defjvp, defjvp_argnums
 
 wrap_namespace(scipy.linalg.__dict__, globals())  # populates module namespace
 
@@ -39,3 +39,28 @@ def _jvp_sqrtm(dA, ans, A, disp=True, blocksize=64):
     assert disp, "sqrtm jvp not implemented for disp=False"
     return solve_sylvester(ans, ans, dA)
 defjvp(sqrtm, _jvp_sqrtm)
+
+def _jvp_sylvester(argnums, dms, ans, args, _):
+    a, b, q = args
+    if 0 in argnums:
+        da = dms[0]
+        db = dms[1] if 1 in argnums else 0
+    else:
+        da = 0
+        db = dms[0] if 1 in argnums else 0
+    dq = dms[-1] if 2 in argnums else 0
+    rhs = dq - anp.dot(da, ans) - anp.dot(ans, db)
+    return solve_sylvester(a, b, rhs)
+defjvp_argnums(solve_sylvester, _jvp_sylvester)
+
+def _vjp_sylvester(argnums, ans, args, _):
+    a, b, q = args
+    def vjp(g):
+        vjps = []
+        q_vjp = solve_sylvester(anp.transpose(a), anp.transpose(b), g)
+        if 0 in argnums: vjps.append(-anp.dot(q_vjp, anp.transpose(ans)))
+        if 1 in argnums: vjps.append(-anp.dot(anp.transpose(ans), q_vjp))
+        if 2 in argnums: vjps.append(q_vjp)
+        return tuple(vjps)
+    return vjp
+defvjp_argnums(solve_sylvester, _vjp_sylvester)
--- tests/test_scipy.py
+++ tests/test_scipy.py
@@ -212,4 +212,6 @@ else:
                                                  [(R(3), R(3))])
 
     ## Linalg
-    def test_sqrtm(): combo_check(spla.sqrtm, modes=['fwd', 'rev'], order=1)([R(3, 3)])
+    def test_sqrtm(): combo_check(spla.sqrtm, modes=['fwd'], order=2)([R(3, 3)])
+    def test_sqrtm(): combo_check(symmetrize_matrix_arg(spla.sqrtm, 0), modes=['fwd', 'rev'], order=2)([R(3, 3)])
+    def test_solve_sylvester(): combo_check(spla.solve_sylvester, [0, 1, 2], modes=['rev', 'fwd'], order=2)([R(3, 3)], [R(3, 3)], [R(3, 3)])
