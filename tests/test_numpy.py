Fix test for np.gradient

tests/test_numpy.py
@@ -698,13 +698,11 @@ def test_astype():
     assert grad(f)(x).dtype == np.dtype('float32')
 
 def test_gradient():
-    check_grads(np.gradient, 0, modes=['rev'], order=1)(npr.randn(10))
-    check_grads(np.gradient, 0, modes=['rev'], order=1)(npr.randn(10, 10))
-    check_grads(np.gradient, 0, modes=['rev'], order=1)(npr.randn(10, 10, 10))
+    check_grads(np.gradient, 0, order=1)(npr.randn(10))
+    check_grads(np.gradient, 0, order=1)(npr.randn(10, 10))
+    check_grads(np.gradient, 0, order=1)(npr.randn(10, 10, 10))
 
     for a in [None, 0, 1, -1, (0, 1), (0, -1)]:
-        check_grads(
-            lambda x: np.gradient(x, axis=a), 
-            0, order=1)(npr.randn(10, 10, 10))
+        check_grads(np.gradient, 0, order=1)(npr.randn(10, 10, 10), axis=a)
 
 