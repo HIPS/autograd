31a8c8ac4d8196f384ab3389df5c125bde777c2c
momchilmm <momchilmm@gmail.com>
2019-07-30 16:56:42 -0700

    Incorporating numerically computed vjp-s in the gradient computation

--- autograd/core.py
+++ autograd/core.py
@@ -3,7 +3,6 @@ from functools import reduce
 from .tracer import trace, primitive, toposort, Node, Box, isbox, getval
 from .util import func, subval
 
-
 # -------------------- reverse mode --------------------
 
 def make_vjp(fun, x):
