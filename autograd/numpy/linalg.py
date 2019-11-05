7ff112577343283156370a6521840ce8c2e09408
refraction-ray <kcanamgal@foxmail.com>
2019-11-05 16:20:22 +0800

    add reverse ad for general eig

--- autograd/numpy/linalg.py
+++ autograd/numpy/linalg.py
@@ -21,6 +21,14 @@ _dot = partial(anp.einsum, '...ij,...jk->...ik')
 # batched diag 
 _diag = lambda a: anp.eye(a.shape[-1])*a 
 
+# batched diagonal, similar to matrix_diag in tensorflow
+def _matrix_diag(a):
+    reps = anp.array(a.shape)
+    reps[:-1] = 1
+    reps[-1] = a.shape[-1]
+    newshape = list(a.shape) + [a.shape[-1]]
+    return _diag(anp.tile(a, reps).reshape(newshape))
+
 # add two dimensions to the end of x
 def add2d(x): return anp.reshape(x, anp.shape(x) + (1, 1))
 
@@ -140,6 +148,27 @@ def grad_eigh(ans, x, UPLO='L'):
 
     return vjp
 defvjp(eigh, grad_eigh)
+
+# https://arxiv.org/pdf/1701.00392.pdf Eq(4.77)
+# Note the formula from Sec3.1 in https://people.maths.ox.ac.uk/gilesm/files/NA-08-01.pdf is incomplete
+def grad_eig(ans, x):
+    """Gradient of a general square (complex valued) matrix"""
+    e, u = ans # eigenvalues as 1d array, eigenvectors in columns
+    n = e.shape[-1]
+    def vjp(g):
+        ge, gu = g
+        ge = _matrix_diag(ge)
+        f = 1/(e[..., anp.newaxis, :] - e[..., :, anp.newaxis] + 1.e-20)
+        f -= _diag(f)
+        ut = anp.swapaxes(u, -1, -2)
+        r = inv(ut)@(ge+f*(ut@gu) - f*(ut@anp.conj(u)@(anp.real(ut@gu)*anp.eye(n))))@ut
+        if not anp.iscomplexobj(x):
+            r = anp.real(r)
+            # the derivative is still complex for real input (imaginary delta is allowed), real output
+            # but the derivative should be real in real input case when imaginary delta is forbidden
+        return r
+    return vjp
+defvjp(eig, grad_eig)
 
 def grad_cholesky(L, A):
     # Based on Iain Murray's note http://arxiv.org/abs/1602.07527
