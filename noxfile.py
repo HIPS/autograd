7c77f3ce6604ba9f7e385e426a0369d6b5ca4845
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-08-25 03:54:26 +0530

    Create an initial draft for the Noxfile

---
+++ noxfile.py
@@ -0,0 +1,34 @@
+import nox
+
+nox.needs_version = ">=2024.4.15"
+nox.options.default_venv_backend = "uv|virtualenv"
+nox.options.reuse_existing_virtualenvs = True
+nox.options.error_on_external_run = True
+nox.options.sessions = ["lint", "validate-package", "tests"]
+nox.options.sessions = ["tests"]
+
+
+@nox.session(name="validate-package")
+def check(session):
+    """Build source distribution, wheel, and check their metadata"""
+    session.install("build", "twine")
+    session.run('python', '-m', 'build')
+    session.run('twine', 'check', '--strict', "dist/*")
+
+
+@nox.session(name="tests")
+def py27(session):
+    """Run unit tests and generate a coverage report"""
+    session.install("-e", ".[scipy,test]")
+    session.run("pytest", "--cov=autograd", "--cov-report=xml")
+
+
+# TODO: Replace with pre-commit and pre-commit.ci
+# TODO: Fix style failures
+@nox.session(name="lint")
+def ruff(session):
+    """Lightning-fast linting for Python"""
+    session.install("ruff")
+    session.run('ruff', 'check', '.')
+    session.notify("tests")
+
