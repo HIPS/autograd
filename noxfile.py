3df0f286e6db7eee4fe72f456565c24502f91e9f
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-08-28 22:09:21 +0530

    Implement testing against NumPy, SciPy nightlies

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,14 @@ import platform
 
 import nox
 
+NIGHTLY_INDEX_URL = "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple"
+UV_NIGHTLY_ENV_VARS = {
+    "UV_INDEX_URL": NIGHTLY_INDEX_URL,
+    "UV_PRERELEASE": "allow",
+    "UV_INDEX_STRATEGY": "first-index",
+    "UV_NO_CACHE": "true"
+}
+
 nox.needs_version = ">=2024.4.15"
 nox.options.default_venv_backend = "uv|virtualenv"
 nox.options.reuse_existing_virtualenvs = False
@@ -14,11 +22,11 @@ nox.options.sessions = ["tests"]
 def check(session):
     """Build source distribution, wheel, and check their metadata"""
     session.install("build", "twine", silent=False)
-    session.run('python', '-m', 'build')
-    session.run('twine', 'check', '--strict', "dist/*")
+    session.run("python", "-m", "build")
+    session.run("twine", "check", "--strict", "dist/*")
 
 
-@nox.session(name="tests")
+@nox.session(name="tests", tags=["tests"])
 def run_tests(session):
     """Run unit tests and generate a coverage report"""
     # SciPy doesn't have wheels on PyPy
@@ -35,5 +43,18 @@ def run_tests(session):
 def ruff(session):
     """Lightning-fast linting for Python"""
     session.install("ruff", silent=False)
-    session.run('ruff', 'check', '.')
+    session.run("ruff", "check", ".")
     session.notify("tests")
+
+
+@nox.session(name="nightly-tests", tags=["tests"])
+def run_nightly_tests(session):
+    """Run tests against nightly versions of dependencies"""
+    session.install("-e", ".[test]", silent=False)
+    # SciPy doesn't have wheels on PyPy
+    if platform.python_implementation() == "PyPy":
+        session.install("numpy", "--upgrade", silent=False, env=UV_NIGHTLY_ENV_VARS)
+    else:
+        session.install(
+            "numpy", "scipy", "--upgrade", silent=False, env=UV_NIGHTLY_ENV_VARS)
+    session.run("pytest", "--cov=autograd", "--cov-report=xml", "--cov-append", *session.posargs)
