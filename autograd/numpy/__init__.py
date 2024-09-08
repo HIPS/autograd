25d8818d4758cd49fe1a23f3fcfab52203212dee
Fabian Joswig <fjosw@users.noreply.github.com>
2024-09-08 22:33:23 +0200

    [Feat] Add support for index tricks in autograd numpy

--- autograd/numpy/__init__.py
+++ autograd/numpy/__init__.py
@@ -1,2 +1,7 @@
 from . import fft, linalg, numpy_boxes, numpy_jvps, numpy_vjps, numpy_vspaces, random
 from .numpy_wrapper import *
+from numpy import (
+    diag_indices_from, diag_indices, fill_diagonal, ndindex, ndenumerate,
+    ix_, c_, r_, s_, ogrid, mgrid, unravel_index, ravel_multi_index,
+    index_exp
+)
