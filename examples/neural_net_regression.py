6c7d3acb0a63466c2d5b6bd811d5a393f9c7086e
Jakob van Santen <jvansanten@gmail.com>
2025-07-22 10:13:17 +0200

    Floor divide

--- examples/neural_net_regression.py
+++ examples/neural_net_regression.py
@@ -38,7 +38,7 @@ def logprob(weights, inputs, targets, noise_scale=0.1):
 
 def build_toy_dataset(n_data=80, noise_std=0.1):
     rs = npr.RandomState(0)
-    inputs = np.concatenate([np.linspace(0, 3, num=n_data / 2), np.linspace(6, 8, num=n_data / 2)])
+    inputs = np.concatenate([np.linspace(0, 3, num=n_data // 2), np.linspace(6, 8, num=n_data // 2)])
     targets = np.cos(inputs) + rs.randn(n_data) * noise_std
     inputs = (inputs - 4.0) / 2.0
     inputs = inputs[:, np.newaxis]
