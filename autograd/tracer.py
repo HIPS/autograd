b2fc2657c705aeb1506be5b7b417311a83f90cd1
Dan Raviv <dan@soundradix.com>
2026-06-19 22:46:36 -0700

    Improve autograd integration with ufuncs and array-like containers (#717)
    Specifically, allows doing real work with `xarray.DataArray`s
    containing `ArrayBox`es

--- autograd/tracer.py
+++ autograd/tracer.py
@@ -2,6 +2,10 @@ import warnings
 from collections import defaultdict
 from contextlib import contextmanager
 
+import numpy as np
+
+import autograd
+
 from .util import subvals, toposort
 from .wrap_util import wraps
 
@@ -33,28 +37,64 @@ class Node:
         return root
 
 
+trace_primitives_map = {}
+
+
 def primitive(f_raw):
     """
     Wraps a function so that its gradient can be specified and its invocation
     can be recorded. For examples, see the docs."""
 
+    is_ufunc = isinstance(f_raw, np.ufunc)
+
     @wraps(f_raw)
-    def f_wrapped(*args, **kwargs):
-        boxed_args, trace, node_constructor = find_top_boxed_args(args)
+    def f_wrapped(*args, called_by_autograd_dispatcher=False, **kwargs):
+        boxed_args, trace, node_constructor, ufunc_dispatch_needed = find_top_boxed_args(args)
         if boxed_args:
+            # If we are a wrapper around a ufunc, and if there is at least one ArrayBox
+            # argument, then first forward further handling to the ufunc dispatching mechanism
+            # (if we aren't already running inside it after being called by the ArrayBox
+            # __array_ufunc__ function). This allows other operands which define __array_ufunc__
+            # to also try to handle the call.
+            #
+            # (It's possible our handling attempt below will get the first shot; the handlers
+            # order is determined by the dispatch mechanism. Also, if no other array-like
+            # arguments are interested in handling the ufunc, then we don't defer to the
+            # dispatch mechanism because there is no point).
+            #
+            # For example, consider multiplying an ndarray wrapped inside an ArrayBox
+            # by an xarray.DataArray. The handling below will fail: The ndarray will
+            # be unboxed and multiplied by the DataArray resulting in a DataArray,
+            # for which `new_box` will raise an exception. In contrast, the DataArray's
+            # handling of the call might succeed: it might contain an ndarray, either
+            # plain or boxed in an ArrayBox, in which case it will be multiplied by
+            # the other ArrayBox yielding a new ArrayBox, which will be stored in a new
+            # DataArray.
+            if is_ufunc and ufunc_dispatch_needed and not called_by_autograd_dispatcher:
+                return f_raw(*args, **kwargs)
+
             argvals = subvals(args, [(argnum, box._value) for argnum, box in boxed_args])
             if f_wrapped in notrace_primitives[node_constructor]:
-                return f_wrapped(*argvals, **kwargs)
+                return f_wrapped(
+                    *argvals, called_by_autograd_dispatcher=called_by_autograd_dispatcher, **kwargs
+                )
             parents = tuple(box._node for _, box in boxed_args)
             argnums = tuple(argnum for argnum, _ in boxed_args)
-            ans = f_wrapped(*argvals, **kwargs)
+            ans = f_wrapped(*argvals, called_by_autograd_dispatcher=called_by_autograd_dispatcher, **kwargs)
             node = node_constructor(ans, f_wrapped, argvals, kwargs, argnums, parents)
-            return new_box(ans, trace, node)
+            try:
+                box = new_box(ans, trace, node)
+                return box
+            except Exception as e:
+                if called_by_autograd_dispatcher:
+                    raise NotImplementedError from e
+                raise
         else:
             return f_raw(*args, **kwargs)
 
     f_wrapped.fun = f_raw
     f_wrapped._is_autograd_primitive = True
+    trace_primitives_map[f_raw] = f_wrapped
     return f_wrapped
 
 
@@ -79,8 +119,13 @@ def find_top_boxed_args(args):
     top_trace = -1
     top_boxes = []
     top_node_type = None
+    any_arraybox = False
+    any_unboxed_ufunc_overrider = False
     for argnum, arg in enumerate(args):
-        if isbox(arg):
+        t = type(arg)
+        if t in box_types:
+            if t == autograd.numpy.numpy_boxes.ArrayBox:
+                any_arraybox = True
             trace = arg._trace
             if trace > top_trace:
                 top_boxes = [(argnum, arg)]
@@ -88,7 +133,10 @@ def find_top_boxed_args(args):
                 top_node_type = type(arg._node)
             elif trace == top_trace:
                 top_boxes.append((argnum, arg))
-    return top_boxes, top_trace, top_node_type
+        elif getattr(t, "__array_ufunc__", None) not in (None, np.ndarray.__array_ufunc__):
+            any_unboxed_ufunc_overrider = True
+    ufunc_dispatch_needed = any_arraybox and any_unboxed_ufunc_overrider
+    return top_boxes, top_trace, top_node_type, ufunc_dispatch_needed
 
 
 class TraceStack:
